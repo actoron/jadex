014ac717af5b211d430b0056e6e274c5b43e826d
Legacy SVNUser <nobody@nowhere>
2010-02-05 16:59:05 +0000

    - typo

--- jadex-bpmn-editor/jadex.tools.bpmn.editor/src/jadex/tools/bpmn/editor/properties/JadexBpmnDiagramArgumentsSection.java
+++ jadex-bpmn-editor/jadex.tools.bpmn.editor/src/jadex/tools/bpmn/editor/properties/JadexBpmnDiagramArgumentsSection.java
@@ -12,7 +12,7 @@ public class JadexBpmnDiagramArgumentsSection extends
 		Abstract4ColumnTablePropertySection
 {
 
-	public static final String label = "Arguents";
+	public static final String label = "Arguments";
 	public static final String[] fields = new String[] {"Name", "Description", "Typename", "Value"};
 	public static final int[] columnWeights = new int[] {1,1,1,8};
 	public static final String[] defaultListElementAttributeValues = new String[]{"name", "description", "Object", ""};
