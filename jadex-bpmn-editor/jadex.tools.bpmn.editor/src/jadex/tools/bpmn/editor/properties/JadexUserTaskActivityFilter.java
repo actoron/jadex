8dd55fcc983f75f8230ddb9c5a01f0082d8d1ce9
Legacy SVNUser <nobody@nowhere>
2010-02-08 15:00:57 +0000

    - added SubProcess***EditPart to ActivityFilter

--- jadex-bpmn-editor/jadex.tools.bpmn.editor/src/jadex/tools/bpmn/editor/properties/JadexUserTaskActivityFilter.java
+++ jadex-bpmn-editor/jadex.tools.bpmn.editor/src/jadex/tools/bpmn/editor/properties/JadexUserTaskActivityFilter.java
@@ -10,6 +10,10 @@ import org.eclipse.stp.bpmn.Activity;
 import org.eclipse.stp.bpmn.ActivityType;
 import org.eclipse.stp.bpmn.diagram.edit.parts.Activity2EditPart;
 import org.eclipse.stp.bpmn.diagram.edit.parts.ActivityEditPart;
+import org.eclipse.stp.bpmn.diagram.edit.parts.SubProcessEditPart;
+import org.eclipse.stp.bpmn.diagram.edit.parts.SubProcessNameEditPart;
+import org.eclipse.stp.bpmn.diagram.edit.parts.SubProcessSubProcessBodyCompartmentEditPart;
+import org.eclipse.stp.bpmn.diagram.edit.parts.SubProcessSubProcessBorderCompartmentEditPart;
 
 /**
  * @author Claas
@@ -24,6 +28,13 @@ public class JadexUserTaskActivityFilter implements IFilter
 	@Override
 	public boolean select(Object toTest)
 	{
+		if (toTest instanceof SubProcessEditPart || toTest instanceof SubProcessNameEditPart
+				|| toTest instanceof SubProcessSubProcessBodyCompartmentEditPart || toTest instanceof SubProcessSubProcessBorderCompartmentEditPart)
+		{
+			return true;
+		}
+		
+		
 		if (toTest instanceof ActivityEditPart || toTest instanceof Activity2EditPart)
 		{
 			EditPart part = (EditPart) toTest;
