da369607a711de0d3cbdc9adc95da378e01c3a9a
Legacy SVNUser <nobody@nowhere>
2011-03-10 15:18:06 +0000

    some comments for the micro agent coordination example using tuple spaces

--- sodekovs-coordination/src/main/java/deco4mas/examples/micro/tspaces/ReceiverAgent.java
+++ sodekovs-coordination/src/main/java/deco4mas/examples/micro/tspaces/ReceiverAgent.java
@@ -7,8 +7,14 @@ import jadex.micro.MicroAgent;
 import java.util.Map;
 
 import deco4mas.coordinate.micro.CoordinateComponentStep;
+import deco4mas.examples.micro.tspaces.SenderAgent.CounterIncrementStep;
 
 /**
+ * The {@link InformCounterIncrementStep} in this class is called whenever the
+ * coordination framework detects the that the {@link CounterIncrementStep} in
+ * the {@link SenderAgent} was started. The coordination framework then starts
+ * the {@link InformCounterIncrementStep}.
+ * 
  * @author Thomas Preisler
  */
 public class ReceiverAgent extends MicroAgent {
@@ -20,7 +26,7 @@ public class ReceiverAgent extends MicroAgent {
 
 	@Override
 	public void executeBody() {
-
+		// does nothing
 	}
 
 	@Override
@@ -28,18 +34,33 @@ public class ReceiverAgent extends MicroAgent {
 		return super.agentKilled();
 	}
 
+	/**
+	 * This step is called from the coordination framework. The class has to
+	 * extend {@link CoordinateComponentStep} to make sure that the constructor
+	 * is implemented. The coordination parameter are passed to this step over
+	 * the {@link Map} argument in the constructor.
+	 * 
+	 * @author Thomas Preisler
+	 */
 	public class InformCounterIncrementStep extends CoordinateComponentStep {
 
+		private Integer counter = 0;
+
+		/**
+		 * Constructor.
+		 * 
+		 * @param coordinatenParameter
+		 *            a {@link Map} containing the coordination parameters.
+		 */
 		public InformCounterIncrementStep(Map<String, Object> coordinatenParameter) {
 			super(coordinatenParameter);
 
 			this.counter = (Integer) coordinatenParameter.get("counter");
 		}
 
-		private Integer counter = 0;
-
 		@Override
 		public Object execute(IInternalAccess ia) {
+			// just print the current value of counter
 			System.out.println("***** InformCounterIncrementStep called: " + counter);
 			return null;
 		}
--- sodekovs-coordination/src/main/java/deco4mas/examples/micro/tspaces/SenderAgent.java
+++ sodekovs-coordination/src/main/java/deco4mas/examples/micro/tspaces/SenderAgent.java
@@ -7,10 +7,21 @@ import jadex.micro.MicroAgent;
 import deco4mas.coordinate.annotation.CoordinationParameter;
 
 /**
+ * This class is under observation by the coordination framework. Whenever the
+ * {@link CounterIncrementStep} is started this is event is observed by the
+ * coordination framework and the attribute {@link SenderAgent#counter} is
+ * propagated to the {@link ReceiverAgent}.
+ * 
  * @author Thomas Preisler
  */
 public class SenderAgent extends MicroAgent {
 
+	/**
+	 * This attribute is propagated through the coordination framework. It has
+	 * to be public so it could be read using reflection in the coordination
+	 * framework. The annotation is currently optional and only helps to
+	 * understand the code better.
+	 */
 	@CoordinationParameter(steps = { "CounterIncrementStep" })
 	public Integer counter = 0;
 
@@ -21,6 +32,7 @@ public class SenderAgent extends MicroAgent {
 
 	@Override
 	public void executeBody() {
+		// wait 7s then start the first step
 		waitFor(7000, new CounterIncrementStep());
 	}
 
@@ -29,6 +41,13 @@ public class SenderAgent extends MicroAgent {
 		return super.agentKilled();
 	}
 
+	/**
+	 * Whenever this step is started the coordination framework observes this
+	 * event and propagates the {@link SenderAgent#counter} attribute to the
+	 * {@link ReceiverAgent}.
+	 * 
+	 * @author Thomas Preisler
+	 */
 	public class CounterIncrementStep implements IComponentStep {
 
 		/*
@@ -39,11 +58,13 @@ public class SenderAgent extends MicroAgent {
 		 */
 		@Override
 		public Object execute(IInternalAccess ia) {
+			// only execute it 5 times
 			if (counter < 5) {
 				counter++;
 				System.out.println("***** Executing CounterIncrementStep: " + counter);
 
 				if (counter < 5) {
+					// wait for 2,5s then start the next step
 					waitFor(2500, new CounterIncrementStep());
 				}
 			}
@@ -51,10 +72,13 @@ public class SenderAgent extends MicroAgent {
 			return null;
 		}
 
-		/*
-		 * (non-Javadoc)
+		/**
+		 * Overrides the toString-method and returns the name of the class, this
+		 * name is used as an identification of the step in the coordination
+		 * framework, so it is important that the returned {@link String}
+		 * matches the steps class name.
 		 * 
-		 * @see java.lang.Object#toString()
+		 * @return the steps class name
 		 */
 		@Override
 		public String toString() {
