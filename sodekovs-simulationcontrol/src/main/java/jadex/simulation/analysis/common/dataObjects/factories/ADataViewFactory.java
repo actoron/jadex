d601ee5d65aa17af5c7874aa7e403cb891d48d43
Legacy SVNUser <nobody@nowhere>
2011-05-16 12:05:59 +0000

    updated version

---
+++ sodekovs-simulationcontrol/src/main/java/jadex/simulation/analysis/common/dataObjects/factories/ADataViewFactory.java
@@ -0,0 +1,66 @@
+package jadex.simulation.analysis.common.dataObjects.factories;
+
+import jadex.simulation.analysis.common.dataObjects.ADataObjectView;
+import jadex.simulation.analysis.common.dataObjects.AExperiment;
+import jadex.simulation.analysis.common.dataObjects.AExperimentView;
+import jadex.simulation.analysis.common.dataObjects.AModel;
+import jadex.simulation.analysis.common.dataObjects.AModelView;
+import jadex.simulation.analysis.common.dataObjects.IADataView;
+import jadex.simulation.analysis.common.dataObjects.IAExperiment;
+import jadex.simulation.analysis.common.dataObjects.IAModel;
+import jadex.simulation.analysis.common.dataObjects.parameter.ABasicParameter;
+import jadex.simulation.analysis.common.dataObjects.parameter.ABasicParameterView;
+import jadex.simulation.analysis.common.dataObjects.parameter.AParameterEnsemble;
+import jadex.simulation.analysis.common.dataObjects.parameter.AParameterEnsembleView;
+import jadex.simulation.analysis.common.dataObjects.parameter.IAParameterEnsemble;
+import jadex.simulation.analysis.common.events.data.IADataObservable;
+
+public class ADataViewFactory
+{
+	public static IADataView createView(IADataObservable dataObject)
+	{
+		IADataView view;
+		if (dataObject instanceof ABasicParameter)
+		{
+			view = createParameterView((ABasicParameter)dataObject);
+		} else if (dataObject instanceof IAParameterEnsemble)
+		{
+			view = createParameterEnsembleView((AParameterEnsemble)dataObject);
+		} else if (dataObject instanceof IAModel)
+		{
+			view = createModelView((AModel)dataObject);
+		} else if (dataObject instanceof IAExperiment)
+		{
+			view = createExperimentalFrameView((AExperiment)dataObject);
+		}  else
+		{
+			view = createDataObjectView(dataObject);
+		}
+		return view;
+	}
+
+	public static AParameterEnsembleView createParameterEnsembleView(AParameterEnsemble dataObject)
+	{
+			return new AParameterEnsembleView(dataObject);
+	}
+
+	public static AModelView createModelView(AModel dataObject)
+	{
+		return new AModelView(dataObject);		
+	}
+
+	public static AExperimentView createExperimentalFrameView(AExperiment dataObject)
+	{
+		return new AExperimentView(dataObject);				
+	}
+
+	public static ADataObjectView createDataObjectView(IADataObservable dataObject)
+	{
+		return new ADataObjectView(dataObject);				
+	}
+
+	public static ABasicParameterView createParameterView(ABasicParameter dataObject)
+	{
+		return new ABasicParameterView(dataObject);				
+	}
+}
