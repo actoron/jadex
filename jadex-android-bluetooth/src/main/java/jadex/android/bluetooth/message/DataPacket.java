7fd3004c63ecc687b5b28e74f13a314643bb8b7a
Legacy SVNUser <nobody@nowhere>
2011-10-11 12:28:52 +0000

    jadex-android-bluetooth: refactoring #2 for better flexibility

---
+++ jadex-android-bluetooth/src/main/java/jadex/android/bluetooth/message/DataPacket.java
@@ -0,0 +1,183 @@
+package jadex.android.bluetooth.message;
+
+import jadex.android.bluetooth.device.BluetoothAdapterFactory;
+import jadex.android.bluetooth.device.BluetoothDeviceFactory;
+import jadex.android.bluetooth.device.IBluetoothDevice;
+
+import java.util.UUID;
+
+public class DataPacket {
+	
+	public static byte TYPE_PING = 0;
+	public static byte TYPE_PONG = 1;
+	
+	public static byte TYPE_DATA = 2;
+	public static byte TYPE_BROADCAST = 3;
+	
+	public static byte TYPE_CONNECT_SYN = 4;
+	public static byte TYPE_CONNECT_ACK = 5;
+
+	public static byte TYPE_ROUTING_INFORMATION = 6;
+	
+	public byte Type;
+	public String Src;
+	public String Dest;
+	public byte SeqNo;
+
+	public String pktId;
+	public byte HopCount;
+
+	public short dataSize;
+	
+	public byte[] data;
+	
+	public static final int PACKET_SIZE = 1024;
+	public static final int HEADER_SIZE = 1+17+17 +1 +2+1;
+	public static final int DATA_MAX_SIZE = PACKET_SIZE-HEADER_SIZE;
+
+	public DataPacket(BluetoothMessage msg, byte type) {
+		this(msg.getRemoteAdress(), msg.getData(), type);
+	}
+	
+	public DataPacket(IBluetoothDevice dev, byte[] data, byte type) {
+		this(dev.getAddress(), data, type);
+	}
+	
+	public DataPacket(String dest, byte[] data, byte type) {
+		this.Type = type;
+		this.data = data;
+		if (data != null) {
+			this.dataSize = (short) data.length;
+		} else {
+			this.dataSize = 0;
+		}
+		if (dest != null) {
+			this.Dest = dest;
+		}
+		this.Src = BluetoothAdapterFactory.getBluetoothAdapter().getAddress();
+		newPaketID();
+	}
+
+	public DataPacket(byte[] buffer) {
+		this.Type = buffer[0];
+		this.Src = new String(buffer, 1, 17);
+		this.Dest = new String(buffer, 18, 17);
+		this.pktId = new String(buffer, 35, 21);
+		this.HopCount = buffer[56];
+		byte[] dataSize = new byte[] { buffer[57], buffer[58] };
+		
+		this.dataSize = readShort(dataSize, 0);
+		this.SeqNo = buffer[59];
+		this.data = new byte[this.dataSize];
+
+		for (int i = 0; i < this.dataSize; i++) {
+			this.data[i] = buffer[i + 60];
+		}
+
+		dataSize = null;
+		buffer = null;
+	}
+
+	public byte[] asByteArray() {
+		byte header[] = new byte[1];
+		header[0] = this.Type;
+		byte packetAsBytes[] = joinByteArray(header, this.Src.getBytes());
+		packetAsBytes = joinByteArray(packetAsBytes, this.Dest.getBytes());
+
+		packetAsBytes = joinByteArray(packetAsBytes, this.pktId.getBytes());
+		byte hop[] = new byte[1];
+		hop[0] = this.HopCount;
+		packetAsBytes = joinByteArray(packetAsBytes, hop);
+
+		byte[] shortToByteArray = shortToByteArray(this.dataSize);
+		packetAsBytes = joinByteArray(packetAsBytes, shortToByteArray);
+
+		byte seq[] = new byte[1];
+		seq[0] = this.SeqNo;
+		packetAsBytes = joinByteArray(packetAsBytes, seq);
+
+		packetAsBytes = joinByteArray(packetAsBytes, this.data);
+		// //add a stop
+		// packetAsBytes = joinByteArray(packetAsBytes," ".getBytes());
+		// packetAsBytes[packetAsBytes.length-1] = 0;
+
+		header = null;
+		hop = null;
+		seq = null;
+
+		return packetAsBytes;
+	}
+
+	private byte[] joinByteArray(byte[] a, byte[] b) {
+		if (a == null) {
+			return b;
+		} else if (b == null) {
+			return a;
+		}
+		int count = a.length + b.length;
+		byte[] result = new byte[count];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = a[i];
+		}
+		for (int i = 0; i < b.length; i++) {
+			result[a.length + i] = b[i];
+		}
+
+		return result;
+	}
+	
+	public void newPaketID() {
+		this.pktId = UUID.randomUUID().toString();
+		this.pktId = this.pktId.substring(pktId.length() - 21, pktId.length());
+	}
+	
+	public short readShort(byte[] data, int offset) {
+		return (short) (((data[offset] << 8)) | ((data[offset + 1] & 0xff)));
+	}
+
+	public byte[] shortToByteArray(short s) {
+		return new byte[] { (byte) ((s & 0xFF00) >> 8), (byte) (s & 0x00FF) };
+	}
+	
+	public String getDataAsString() {
+		return new String(data);
+	}
+	
+	public IBluetoothDevice getDestinationDevice() {
+		return BluetoothDeviceFactory.createBluetoothDevice(Dest);
+	}
+
+
+	public IBluetoothDevice getSourceDevice() {
+		return BluetoothDeviceFactory.createBluetoothDevice(Src);
+	}
+	
+	@Override
+	public String toString() {
+		String type = "DATA";
+		if (Type == TYPE_PONG) {
+			type = "PONG";
+		} else if (Type == TYPE_PING) {
+			type = "PING";
+		} else if (Type == TYPE_CONNECT_SYN) {
+			type = "SYN";
+		} else if (Type == TYPE_CONNECT_ACK) {
+			type = "ACK";
+		} else if (Type == TYPE_BROADCAST) {
+			type = "BROADCAST";
+		} else if (Type == TYPE_ROUTING_INFORMATION) {
+			type = "ROUTING_INFO";
+		}
+		StringBuilder s = new StringBuilder("From: ");
+		s.append(Src);
+		s.append("\nTo: ");
+		s.append(Dest);
+		s.append("\nType: ");
+		s.append(type);
+		if (data != null || dataSize < 100) {
+			s.append("\nContent: ");
+			s.append(getDataAsString());
+		}
+		return s.toString();
+	}
+}
