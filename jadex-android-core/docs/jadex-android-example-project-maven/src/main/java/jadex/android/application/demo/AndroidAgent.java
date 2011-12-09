401209694955ab556305d0c445c6dce54ae27280
Alexander Pokahr <ap@activecomponents.org>
2011-12-09 17:52:47 +0000

    Added ACL message handling to example (can be tested with remote JCC)

--- jadex-android-core/docs/jadex-android-example-project-maven/src/main/java/jadex/android/application/demo/AndroidAgent.java
+++ jadex-android-core/docs/jadex-android-example-project-maven/src/main/java/jadex/android/application/demo/AndroidAgent.java
@@ -1,24 +1,58 @@
 package jadex.android.application.demo;
 
+import java.util.Map;
+
+import jadex.base.fipa.SFipa;
+import jadex.bridge.service.types.message.MessageType;
 import jadex.commons.future.IFuture;
 import jadex.micro.MicroAgent;
 import android.os.Bundle;
 import android.os.Message;
 
-public class AndroidAgent extends MicroAgent {
-
-	@Override
-	public IFuture agentCreated() {
-		// TODO Auto-generated method stub
-		return super.agentCreated();
+/**
+ *  Simple example agent that shows messages
+ *  when it is started, stopped and when it receives a message. 
+ */
+public class AndroidAgent extends MicroAgent
+{
+	//-------- methods --------
+	
+	/**
+	 *  Called when the agent is started.
+	 */
+	public void executeBody()
+	{
+		showAndroidMessage("This is Agent <<" + this.getAgentName() + ">> saying hello!");
 	}
 
-	@Override
-	public void executeBody() {
-		super.executeBody();
+	/**
+	 *  Called when the agent is killed.
+	 */
+	public IFuture<Void> agentKilled()
+	{
+		showAndroidMessage("This is Agent <<" + this.getAgentName() + ">> saying goodbye!");
+		return IFuture.DONE;
+	}
+
+	/**
+	 *  Called when the agent receives a message.
+	 */
+	public void messageArrived(Map<String, Object> msg, MessageType mt)
+	{
+		showAndroidMessage(msg.get(SFipa.PERFORMATIVE)+"("+msg.get(SFipa.CONTENT)+")");
+	}
+	
+	//-------- helper methods --------
+
+	/**
+	 *	Show a message on the device.  
+	 *  @param msg The message to be shown.
+	 */
+	protected void showAndroidMessage(String msg)
+	{
 		Message message = new Message();
 		Bundle bundle = new Bundle();
-		bundle.putString("text","This is Agent <<" + this.getAgentName() + ">> saying hello!");
+		bundle.putString("text", msg);
 		message.setData(bundle);
 		JadexAndroidHelloWorldActivity.getHandler().sendMessage(message);
 	}
