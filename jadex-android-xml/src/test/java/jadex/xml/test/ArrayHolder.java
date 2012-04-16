3ff1d664f6e8289f968fed02e6869757db2c290c
Legacy SVNUser <nobody@nowhere>
2012-04-16 17:19:56 +0000

    jadex-android: added xml tests

---
+++ jadex-android-xml/src/test/java/jadex/xml/test/ArrayHolder.java
@@ -0,0 +1,57 @@
+package jadex.xml.test;
+
+import jadex.commons.SUtil;
+
+
+/**
+ *  Object holding a multi array as attribute.
+ */
+public class ArrayHolder
+{
+	/** The array data. */
+	protected int[] data;
+	
+	/**
+	 *  Create an empty array holder.
+	 */
+	public ArrayHolder()
+	{
+		// bean constructor
+	}
+	
+	/**
+	 * Create a new array holder.
+	 */
+	public ArrayHolder(int[] data)
+	{
+		this.data = data;
+	}
+
+	/**
+	 * Get the data.
+	 * 
+	 * @return the data.
+	 */
+	public int[] getData()
+	{
+		return data;
+	}
+
+	/**
+	 * Set the data.
+	 * 
+	 * @param data The data to set.
+	 */
+	public void setData(int[] data)
+	{
+		this.data = data;
+	}
+	
+	/**
+	 *  Test if two array holder are equal.
+	 */
+	public boolean equals(Object obj)
+	{
+		return obj instanceof ArrayHolder && SUtil.arrayEquals(data, ((ArrayHolder)obj).getData());
+	}
+}
