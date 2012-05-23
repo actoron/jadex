67723d202f7683ade4051abd4b00a81e64d187c3
Legacy SVNUser <nobody@nowhere>
2012-05-23 13:54:44 +0000

    Added the replicated message queue example as an extension of the previous message queue example

---
+++ jadex-applications-micro/src/main/java/jadex/micro/examples/messagequeue/replicated/ReplicationSubscription.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package jadex.micro.examples.messagequeue.replicated;
+
+import jadex.commons.future.ISubscriptionIntermediateFuture;
+import jadex.micro.examples.messagequeue.Event;
+
+/**
+ * @author thomas
+ * 
+ */
+public class ReplicationSubscription {
+
+	private IMessageQueueReplicationService service = null;
+
+	private ISubscriptionIntermediateFuture<Event> subscription = null;
+
+	/**
+	 * @param service
+	 * @param subscription
+	 */
+	public ReplicationSubscription(IMessageQueueReplicationService service, ISubscriptionIntermediateFuture<Event> subscription) {
+		super();
+		this.service = service;
+		this.subscription = subscription;
+	}
+
+	/**
+	 * @return the service
+	 */
+	public IMessageQueueReplicationService getService() {
+		return service;
+	}
+
+	/**
+	 * @param service
+	 *            the service to set
+	 */
+	public void setService(IMessageQueueReplicationService service) {
+		this.service = service;
+	}
+
+	/**
+	 * @return the subscription
+	 */
+	public ISubscriptionIntermediateFuture<Event> getSubscription() {
+		return subscription;
+	}
+
+	/**
+	 * @param subscription
+	 *            the subscription to set
+	 */
+	public void setSubscription(ISubscriptionIntermediateFuture<Event> subscription) {
+		this.subscription = subscription;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ReplicationSubscription [" + (service != null ? "service=" + service + ", " : "") + (subscription != null ? "subscription=" + subscription : "") + "]";
+	}
+}
