45f685fdb5fda31e5dca25a5c1d3e6a4ebd3c6d1
Legacy SVNUser <nobody@nowhere>
2012-11-27 18:18:34 +0000

    android: remove old benchmark + test projects

--- jadex-android-applications/jadex-android-application-test/buildDependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mvn clean package install -f ../pom.xml -P jadex-android-artifacts
-if [ "$?" == 0 ]; then
-	mvn -DskipTests clean package install -f ../jadex-android-bluetooth/pom.xml
-fi
-
-if [ "$?" == 0 ]; then
-	./deploy.sh
-fi
--- jadex-android-applications/jadex-android-application-test/src/main/java/de/unihamburg/vsis/jadexAndroid_test/Helper.java
+++
@@ -1,6 +0,0 @@
-package de.unihamburg.vsis.jadexAndroid_test;
-
-
-public class Helper {
-	public static final String LOG_TAG = "jadex";
-}
