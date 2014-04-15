c5ccc9ed4ca76d62d720711037fb658b131f89df
Alex <ap@activecomponents.org>
2014-04-15 16:49:45 +0200

    Added PersistenceComponentManagementService.

---
+++ jadex-platform-extension-management/src/main/java/jadex/platform/service/persistence/PersistenceComponentManagementService.java
@@ -0,0 +1,20 @@
+package jadex.platform.service.persistence;
+
+import jadex.bridge.service.types.factory.IComponentAdapter;
+import jadex.kernelbase.IBootstrapFactory;
+import jadex.platform.service.cms.DecoupledComponentManagementService;
+
+/**
+ *  CMS with additional persistence functionality.
+ */
+public class PersistenceComponentManagementService	extends DecoupledComponentManagementService
+{
+	/**
+	 *  Create a persistence CMS.
+	 */
+	public PersistenceComponentManagementService(IComponentAdapter root, IBootstrapFactory componentfactory,
+		boolean copy, boolean realtime, boolean persist, boolean uniqueids)
+	{
+		super(root, componentfactory, copy, realtime, persist, uniqueids);
+	}	
+}
