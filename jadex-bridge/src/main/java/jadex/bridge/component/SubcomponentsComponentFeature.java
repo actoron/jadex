5d5b1d02d4269aa53a880437ef604d49516f3028
Alex <ap@activecomponents.org>
2014-06-03 18:25:18 +0200

    progress

--- jadex-bridge/src/main/java/jadex/bridge/component/SubcomponentsComponentFeature.java
+++ jadex-bridge/src/main/java/jadex/bridge/component/SubcomponentsComponentFeature.java
@@ -1,15 +1,12 @@
 package jadex.bridge.component;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import jadex.bridge.IComponentIdentifier;
 import jadex.bridge.IInternalAccess;
 import jadex.bridge.modelinfo.ComponentInstanceInfo;
 import jadex.bridge.modelinfo.ConfigurationInfo;
 import jadex.bridge.modelinfo.IModelInfo;
 import jadex.bridge.modelinfo.SubcomponentTypeInfo;
+import jadex.bridge.modelinfo.UnparsedExpression;
 import jadex.bridge.service.RequiredServiceBinding;
 import jadex.bridge.service.search.SServiceProvider;
 import jadex.bridge.service.types.cms.CreationInfo;
@@ -20,6 +17,14 @@ import jadex.commons.future.ExceptionDelegationResultListener;
 import jadex.commons.future.Future;
 import jadex.commons.future.IFuture;
 import jadex.commons.future.IResultListener;
+import jadex.javaparser.SJavaParser;
+import jadex.javaparser.SimpleValueFetcher;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *  This feature provides subcomponents.
@@ -65,13 +70,13 @@ public class SubcomponentsComponentFeature	extends	AbstractComponentFeature	impl
 	{
 		final Future<Void> ret = new Future<Void>();
 		
-		if(config!=null)
+		if(component.getConfiguration()!=null)
 		{
 			final List<IComponentIdentifier> cids = new ArrayList<IComponentIdentifier>();
-			ConfigurationInfo conf = model.getConfiguration(config);
+			ConfigurationInfo conf = component.getModel().getConfiguration(component.getConfiguration());
 			final ComponentInstanceInfo[] components = conf.getComponentInstances();
-			SServiceProvider.getServiceUpwards(getServiceContainer(), IComponentManagementService.class)
-				.addResultListener(createResultListener(new ExceptionDelegationResultListener<IComponentManagementService, Void>(ret)
+			SServiceProvider.getServiceUpwards(component.getServiceProvider(), IComponentManagementService.class)
+				.addResultListener(component.getComponentFeature(IExecutionFeature.class).createResultListener(new ExceptionDelegationResultListener<IComponentManagementService, Void>(ret)
 			{
 				public void customResultAvailable(IComponentManagementService cms)
 				{
@@ -86,7 +91,7 @@ public class SubcomponentsComponentFeature	extends	AbstractComponentFeature	impl
 					// (NOTE2: subcomponents must be created one by one as they
 					// might depend on each other (e.g. bdi factory must be there for jcc)).
 					
-					createComponent(components, cms, model, 0, ret, cids);
+					createComponent(components, cms, component.getModel(), 0, ret, cids);
 				}
 				public void exceptionOccurred(Exception exception)
 				{
@@ -107,13 +112,11 @@ public class SubcomponentsComponentFeature	extends	AbstractComponentFeature	impl
 	 */
 	protected void	createComponent(final ComponentInstanceInfo[] components, final IComponentManagementService cms, final IModelInfo model, final int i, final Future<Void> fut, final List<IComponentIdentifier> cids)
 	{
-		assert !getComponentAdapter().isExternalThread();
-		
 		if(i<components.length)
 		{
 			int num = getNumber(components[i], model);
 			IResultListener<IComponentIdentifier> crl = new CollectionResultListener<IComponentIdentifier>(num, false, 
-				createResultListener(new ExceptionDelegationResultListener<Collection<IComponentIdentifier>, Void>(fut)
+				component.getComponentFeature(IExecutionFeature.class).createResultListener(new ExceptionDelegationResultListener<Collection<IComponentIdentifier>, Void>(fut)
 			{
 				public void customResultAvailable(Collection<IComponentIdentifier> result)
 				{
@@ -137,7 +140,7 @@ public class SubcomponentsComponentFeature	extends	AbstractComponentFeature	impl
 					// todo: rid
 //					System.out.println("curcall: "+getName(components[i], model, j+1)+" "+CallAccess.getCurrentInvocation().getCause());
 					cms.createComponent(getName(components[i], model, j+1), type.getName(),
-						new CreationInfo(components[i].getConfiguration(), getArguments(components[i], model), getComponentAdapter().getComponentIdentifier(),
+						new CreationInfo(components[i].getConfiguration(), getArguments(components[i], model), component.getComponentIdentifier(),
 						suspend, master, daemon, autoshutdown, synchronous, persistable, monitoring, model.getAllImports(), bindings, null), null).addResultListener(crl);
 				}
 				else
@@ -150,5 +153,72 @@ public class SubcomponentsComponentFeature	extends	AbstractComponentFeature	impl
 		{
 			fut.setResult(null);
 		}
+	}
+	
+	/**
+	 *  Get the number of components to start.
+	 *  @return The number.
+	 */
+	protected int getNumber(ComponentInstanceInfo component, IModelInfo model)
+	{
+		Object ret = component.getNumber()!=null? SJavaParser.evaluateExpression(component.getNumber(), model.getAllImports(), this.component.getFetcher(), this.component.getClassLoader()): null;
+		return ret instanceof Integer? ((Integer)ret).intValue(): 1;
+	}
+	
+	/**
+	 *  Get the name of components to start.
+	 *  @return The name.
+	 */
+	protected String getName(ComponentInstanceInfo component, IModelInfo model, int cnt)
+	{
+		String ret = component.getName();
+		if(ret!=null)
+		{
+			SimpleValueFetcher fetcher = new SimpleValueFetcher(this.component.getFetcher());
+			fetcher.setValue("$n", Integer.valueOf(cnt));
+			try
+			{
+				ret = (String)SJavaParser.evaluateExpression(component.getName(), model.getAllImports(), fetcher, this.component.getClassLoader());
+				if(ret==null)
+					ret = component.getName();
+			}
+			catch(RuntimeException e)
+			{
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 *  Get the arguments.
+	 *  @return The arguments as a map of name-value pairs.
+	 */
+	protected Map<String, Object> getArguments(ComponentInstanceInfo component, IModelInfo model)
+	{
+		Map<String, Object> ret = null;		
+		UnparsedExpression[] arguments = component.getArguments();
+		UnparsedExpression argumentsexp = component.getArgumentsExpression();
+		
+		if(arguments.length>0)
+		{
+			ret = new HashMap<String, Object>();
+
+			for(int i=0; i<arguments.length; i++)
+			{
+				// todo: language
+				if(arguments[i].getValue()!=null && arguments[i].getValue().length()>0)
+				{
+					Object val = SJavaParser.evaluateExpression(arguments[i].getValue(), model.getAllImports(), this.component.getFetcher(), this.component.getClassLoader());
+					ret.put(arguments[i].getName(), val);
+				}
+			}
+		}
+		else if(argumentsexp!=null && argumentsexp.getValue()!=null && argumentsexp.getValue().length()>0)
+		{
+			// todo: language
+			ret = (Map<String, Object>)SJavaParser.evaluateExpression(argumentsexp.getValue(), model.getAllImports(), this.component.getFetcher(), this.component.getClassLoader());
+		}
+		
+		return ret;
 	}
 }
