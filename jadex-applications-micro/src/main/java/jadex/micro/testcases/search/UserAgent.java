50f866789e1dd58c9456c2a12ec59fafd93fef3d
Lars Braubach <lb@activecomponents.org>
2014-07-28 16:58:11 +0200

    fix

--- jadex-applications-micro/src/main/java/jadex/micro/testcases/search/UserAgent.java
+++ jadex-applications-micro/src/main/java/jadex/micro/testcases/search/UserAgent.java
@@ -6,6 +6,8 @@ import jadex.bridge.IExternalAccess;
 import jadex.bridge.service.RequiredServiceInfo;
 import jadex.bridge.service.search.SServiceProvider;
 import jadex.bridge.service.types.cms.IComponentManagementService;
+import jadex.commons.future.DefaultTuple2ResultListener;
+import jadex.commons.future.Future;
 import jadex.commons.future.IFuture;
 import jadex.commons.future.ThreadSuspendable;
 import jadex.micro.MicroAgent;
@@ -16,6 +18,7 @@ import jadex.micro.annotation.RequiredService;
 import jadex.micro.annotation.RequiredServices;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Agent
 @RequiredServices({@RequiredService(name = "testService", type = ITestService.class, multiple = true, 
@@ -40,11 +43,33 @@ public class UserAgent
 		IExternalAccess plat = Starter.createPlatform(new String[]{"-gui", "false"}).get(sus);
 		IComponentManagementService cms = SServiceProvider.getService(plat.getServiceProvider(), IComponentManagementService.class, RequiredServiceInfo.SCOPE_PLATFORM).get(sus);
 		
-		for(int i=0; i<2500; i++)
+		final Future<Void> fut = new Future<Void>();
+		
+		final int max = 2500;
+		final int[] cnt = new int[1];
+		for(int i=0; i<max; i++)
 		{
-			cms.createComponent(ProviderAgent.class.getName()+".class", null);
-//			IComponentIdentifier cid = cms.createComponent(ProviderAgent.class.getName()+".class", null).getFirstResult(sus);
-			System.out.println("created: "+i);//+" "+cid);
+			cms.createComponent(ProviderAgent.class.getName()+".class", null).addResultListener(new DefaultTuple2ResultListener<IComponentIdentifier, Map<String, Object>>()
+			{
+				public void firstResultAvailable(IComponentIdentifier result)
+				{
+					cnt[0]++;
+					System.out.println("created: "+result+" "+cnt[0]);
+					if(cnt[0]==max)
+					{
+						fut.setResult(null);
+					}
+				}
+				
+				public void secondResultAvailable(Map<String, Object> result)
+				{
+				}
+				
+				public void exceptionOccurred(Exception exception)
+				{
+					fut.setExceptionIfUndone(exception);
+				}
+			});
 		}
 		
 		cms.createComponent(UserAgent.class.getName()+".class", null).get(sus);
