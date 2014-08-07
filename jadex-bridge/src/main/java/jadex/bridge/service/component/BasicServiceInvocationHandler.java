2087881aca952820e3be4b4b12eb3ca6060d3c9e
Kai Jander <kj@activecomponents.org>
2014-08-07 10:35:16 +0200

    Service properties

--- jadex-bridge/src/main/java/jadex/bridge/service/component/BasicServiceInvocationHandler.java
+++ jadex-bridge/src/main/java/jadex/bridge/service/component/BasicServiceInvocationHandler.java
@@ -12,6 +12,7 @@ import jadex.bridge.service.IInternalService;
 import jadex.bridge.service.IRequiredServiceFetcher;
 import jadex.bridge.service.IService;
 import jadex.bridge.service.IServiceIdentifier;
+import jadex.bridge.service.ProvidedServiceInfo;
 import jadex.bridge.service.RequiredServiceBinding;
 import jadex.bridge.service.RequiredServiceInfo;
 import jadex.bridge.service.annotation.Service;
@@ -48,6 +49,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -428,7 +430,7 @@ public class BasicServiceInvocationHandler implements InvocationHandler, ISwitch
 	 */
 	public static IInternalService createProvidedServiceProxy(IInternalAccess ia, IComponentAdapter adapter, Object service, 
 		String name, Class<?> type, String proxytype, IServiceInvocationInterceptor[] ics, boolean copy, 
-		boolean realtime, IResourceIdentifier rid, boolean monitoring, IResultCommand<Object, Class<?>> componentfetcher)
+		boolean realtime, IResourceIdentifier rid, boolean monitoring, ProvidedServiceInfo info, IResultCommand<Object, Class<?>> componentfetcher)
 	{
 		IInternalService	ret;
 		
@@ -447,7 +449,7 @@ public class BasicServiceInvocationHandler implements InvocationHandler, ISwitch
 		
 		if(!PROXYTYPE_RAW.equals(proxytype) || (ics!=null && ics.length>0))
 		{
-			BasicServiceInvocationHandler handler = createProvidedHandler(name, ia, type, service, realtime, componentfetcher);
+			BasicServiceInvocationHandler handler = createProvidedHandler(name, ia, type, service, realtime, info, componentfetcher);
 			ret	= (IInternalService)Proxy.newProxyInstance(ia.getClassLoader(), new Class[]{IInternalService.class, type}, handler);
 			BasicServiceInvocationHandler.addProvidedInterceptors(handler, service, ics, adapter, ia, proxytype, copy, monitoring, ret.getServiceIdentifier());
 //			ret	= (IInternalService)Proxy.newProxyInstance(ia.getExternalAccess()
@@ -482,12 +484,28 @@ public class BasicServiceInvocationHandler implements InvocationHandler, ISwitch
 	 *  Create a basic invocation handler.
 	 */
 	protected static BasicServiceInvocationHandler createProvidedHandler(String name, IInternalAccess ia, Class<?> type, Object service,
-		boolean realtime, IResultCommand<Object, Class<?>> componentfetcher)
+		boolean realtime, ProvidedServiceInfo info, IResultCommand<Object, Class<?>> componentfetcher)
 	{
+		Map<String, Object> serprops = new HashMap<String, Object>();
+		if (info != null && info.getProperties() != null)
+		{
+			for (UnparsedExpression exp : info.getProperties())
+			{
+				Object val = SJavaParser.parseExpression(exp, ia.getModel().getAllImports(), ia.getClassLoader()).getValue(null);
+				serprops.put(exp.getName(), val);
+			}
+		}
+		
 		BasicServiceInvocationHandler handler;
 		if(service instanceof IService)
 		{
 			IService ser = (IService)service;
+			
+			if (service instanceof BasicService)
+			{
+				((BasicService) service).setPropertyMap(serprops);
+			}
+			
 			handler = new BasicServiceInvocationHandler(ia, ser, ia.getLogger(), realtime, ia.getComponentDescription().getCause(), false);
 //			if(type==null)
 //			{
@@ -524,7 +542,8 @@ public class BasicServiceInvocationHandler implements InvocationHandler, ISwitch
 
 			BasicService mgmntservice = new BasicService(ia.getExternalAccess().getServiceProvider().getId(), type, serclass, null);
 			mgmntservice.createServiceIdentifier(name, service.getClass(), ia.getModel().getResourceIdentifier(), type);
-						
+			mgmntservice.setPropertyMap(serprops);
+			
 			// Do not try to call isAnnotationPresent for Proxy on Android
 			// see http://code.google.com/p/android/issues/detail?id=24846
 			if(!(SReflect.isAndroid() && (service instanceof Proxy))) 
