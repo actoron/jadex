12963ef2f2cb82a3f50d2ada64344c073eec741e
Julian Kalinowski <kalinowski@informatik.uni-hamburg.de>
2015-04-02 17:56:44 +0200

    added @Reference test

--- jadex-platform-standalone-launch/src/test/java/jadex/launch/test/remotereference/LocalServiceProviderAgent.java
+++ jadex-platform-standalone-launch/src/test/java/jadex/launch/test/remotereference/LocalServiceProviderAgent.java
@@ -1,6 +1,10 @@
 package jadex.launch.test.remotereference;
 
+import jadex.bridge.service.annotation.Reference;
 import jadex.bridge.service.annotation.Service;
+import jadex.commons.future.DelegationResultListener;
+import jadex.commons.future.Future;
+import jadex.commons.future.IFuture;
 import jadex.micro.annotation.Agent;
 import jadex.micro.annotation.Implementation;
 import jadex.micro.annotation.Imports;
@@ -16,5 +20,25 @@ import jadex.micro.annotation.ProvidedServices;
 @Service
 public class LocalServiceProviderAgent implements ILocalService
 {
+
+	@Override
+	public IFuture<Void> executeCallback(@Reference ICallback callback)
+	{
+		Future<Void> ret = new Future<Void>();
+		System.out.println("calling back...");
+		IFuture<Void> call = callback.call();
+		call.addResultListener(new DelegationResultListener<Void>(ret));
+		return ret;
+	}
+
+	@Override
+	public IFuture<Void> executeCallback(ICallbackReference callback)
+	{
+		Future<Void> ret = new Future<Void>();
+		System.out.println("calling back...");
+		IFuture<Void> call = callback.call();
+		call.addResultListener(new DelegationResultListener<Void>(ret));
+		return ret;
+	}
 
 }
