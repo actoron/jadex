722eb57d857f8a635ff42d2ca2c9bb2b723149d1
Alexander Pokahr <ap@activecomponents.org>
2015-12-18 16:29:19 +0100

    fixed parallelizer test

--- jadex-platform-extension-management/src/main/java/jadex/platform/service/parallelizer/UserAgent.java
+++ jadex-platform-extension-management/src/main/java/jadex/platform/service/parallelizer/UserAgent.java
@@ -1,14 +1,20 @@
 package jadex.platform.service.parallelizer;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
+import jadex.base.test.TestReport;
 import jadex.base.test.Testcase;
 import jadex.bridge.IInternalAccess;
+import jadex.bridge.component.IArgumentsResultsFeature;
 import jadex.bridge.service.component.IRequiredServicesFeature;
 import jadex.commons.future.ExceptionDelegationResultListener;
 import jadex.commons.future.Future;
 import jadex.commons.future.IFuture;
 import jadex.commons.future.IIntermediateResultListener;
+import jadex.commons.future.IResultListener;
 import jadex.micro.annotation.Agent;
 import jadex.micro.annotation.AgentBody;
 import jadex.micro.annotation.Binding;
@@ -53,29 +59,67 @@ public class UserAgent
 		{
 			public void customResultAvailable(IParallelService paser)
 			{
-				paser.doParallel(new String[]{"a", "b", "c", "d", "e", "f"})
+				String[]	tasks	= new String[]{"a", "b", "c", "d", "e", "f"};
+				final Set<String>	todo	= new HashSet<String>(Arrays.asList(tasks));
+				
+				paser.doParallel(tasks)
 					.addResultListener(new IIntermediateResultListener<String>()
 				{
 					public void intermediateResultAvailable(String result)
 					{
 						System.out.println("ires: "+result);
+						if(!todo.remove(result.substring(11)))	// Strip "result of: "
+						{
+							ret.setExceptionIfUndone(new RuntimeException("Task performed twice: "+result));
+						}
 					}
 					
 					public void finished()
 					{
 						System.out.println("fini");
+						if(todo.isEmpty())
+						{
+							ret.setResultIfUndone(null);
+						}
+						else
+						{
+							ret.setExceptionIfUndone(new RuntimeException("Not all tasks performed: "+todo));
+						}
 					}
 					
 					public void resultAvailable(Collection<String> result)
 					{
 						System.out.println("res: "+result);
+						ret.setExceptionIfUndone(new RuntimeException("No intermediate results."));
 					}
 					
 					public void exceptionOccurred(Exception exception)
 					{
 						System.out.println("ex: "+exception);
+						ret.setExceptionIfUndone(exception);
 					}
 				});
+			}
+		});
+		
+		ret.addResultListener(new IResultListener<Void>()
+		{
+			@Override
+			public void exceptionOccurred(Exception exception)
+			{
+				agent.getComponentFeature(IArgumentsResultsFeature.class).getResults()
+					.put("testresults", new Testcase(1, new TestReport[]{
+						new TestReport("#1", "Test paralellizer", exception)
+					}));
+			}
+			
+			@Override
+			public void resultAvailable(Void result)
+			{
+				agent.getComponentFeature(IArgumentsResultsFeature.class).getResults()
+				.put("testresults", new Testcase(1, new TestReport[]{
+					new TestReport("#1", "Test paralellizer", true, null)
+				}));
 			}
 		});
 			
