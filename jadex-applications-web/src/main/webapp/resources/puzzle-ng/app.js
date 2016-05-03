d950a368e5b544922b99b78a81c7756394f67948
Alexander Pokahr <ap@actoron.com>
2016-05-03 18:17:37 +0200

    puzzle-ng progress

--- jadex-applications-web/src/main/webapp/resources/puzzle-ng/app.js
+++ jadex-applications-web/src/main/webapp/resources/puzzle-ng/app.js
@@ -36,11 +36,36 @@ angular.module('ngPuzzle', []).controller('PuzzleBoard', function($scope, $timeo
 					i==size2	&& j==size2	? "empty" : "";
 			}
 		}
+	};
+	
+	//--- generate the clickable class
+	$scope.clickable	= function clickable(col, row)
+	{
+		check	= $scope.canMove(col, row)!=null;
+		return check ? "clickable" : "";
 	}
 	
-	// start with size 5
+	//--- check if a piece can move ---
+	$scope.canMove	= function canMove(col, row)
+	{
+		ret	= null;
+		// white can move down or left
+		if($scope.board[row][col]=="white")
+		{
+			ret	= row+1<$scope.boardsize && $scope.board[row+1][col]=="empty" ? [row+1, col]
+				: col+1<$scope.boardsize && $scope.board[row][col+1]=="empty" ? [row, col+1] : null;
+		}
+		// red can move up or right
+		else if($scope.board[row][col]=="red")
+		{
+			ret	= row>0 && $scope.board[row-1][col]=="empty" ? [row-1, col]
+				: col>0 && $scope.board[row][col-1]=="empty" ? [row, col-1] : null;
+		}
+		return ret;
+	};
+		
+	// --- init ---
 	$scope.newsize	= 5;
 	$scope.sizes	= [3,5,7,9,11];
-	
 	$scope.restart();
 });
