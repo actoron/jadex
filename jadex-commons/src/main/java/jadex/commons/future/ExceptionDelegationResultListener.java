0ebb25f6555048356fdbfe474731e928a5bb0381
Julian Kalinowski <kalinowski@informatik.uni-hamburg.de>
2016-09-05 14:07:57 +0200

    fix listener

--- jadex-commons/src/main/java/jadex/commons/future/ExceptionDelegationResultListener.java
+++ jadex-commons/src/main/java/jadex/commons/future/ExceptionDelegationResultListener.java
@@ -93,7 +93,7 @@ public abstract class ExceptionDelegationResultListener<E, T> implements IResult
 	 *  Called when the result is available.
 	 * @param result The result.
 	 */
-	public abstract void customResultAvailable(E result);
+	public abstract void customResultAvailable(E result) throws Exception;
 
 	/**
 	 *  Called when an exception occurred.
