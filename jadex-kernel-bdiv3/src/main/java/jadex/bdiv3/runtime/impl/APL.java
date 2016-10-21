850b1817d0eee3fbd9086878fe8d69ffd353ff67
lars <lb@actoron.com>
2016-10-21 14:00:03 +0200

    BDI rebuild flag fixed in combination with exclude modes

--- jadex-kernel-bdiv3/src/main/java/jadex/bdiv3/runtime/impl/APL.java
+++ jadex-kernel-bdiv3/src/main/java/jadex/bdiv3/runtime/impl/APL.java
@@ -59,9 +59,6 @@ public class APL
 	/** The list of candidates. */
 	protected List<Object> candidates;
 	
-	/** The list of candidates. */
-	protected Set<Object> triedcandidates;
-	
 	/** The metagoal. */
 //	protected Object apl_has_metagoal;
 	
@@ -105,6 +102,7 @@ public class APL
 	{
 		final Future<Void> ret = new Future<Void>();
 		
+		// Reset candidates when rebuild
 		if(((MProcessableElement)element.getModelElement()).isRebuild())
 			candidates = null;
 		
@@ -171,22 +169,69 @@ public class APL
 						{
 							candidates.addAll(result);
 						}
+						
+						removeTriedCandidates();
 						ret.setResult(null);
 					}
 				});
 			}
 			else
 			{
+				removeTriedCandidates();
 				ret.setResult(null);
 			}
 		}
 		else
 		{
+//			removeTriedCandidates();
 			ret.setResult(null);
 		}
+		
 		return ret;
 	}
-			
+	
+	/**
+	 *  Remove tried candidates from the actual candidate collection.
+	 */
+	protected void removeTriedCandidates()
+	{
+		MProcessableElement mpe = (MProcessableElement)element.getModelElement();
+		ExcludeMode exclude = mpe.getExcludeMode();
+		
+		if(candidates!=null && candidates.size()>0 && !ExcludeMode.Never.equals(exclude) && element.getTriedPlans()!=null)
+		{
+			List<Object> cands = new ArrayList<Object>(candidates);
+			for(Object candidate: cands)
+			{
+				for(IInternalPlan plan: element.getTriedPlans())
+				{
+					if(plan.getCandidate().equals(candidate))
+					{
+						if(isToExclude(plan, exclude))
+						{
+							candidates.remove(candidate);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 *  Check if an rplan is to exclude wrt the exclude mode and plan result state.
+	 *  @param rplan The tried plan.
+	 *  @param exclude
+	 *  @return True, if should be excluded.
+	 */
+	protected boolean isToExclude(IInternalPlan rplan, ExcludeMode exclude)
+	{
+		return exclude.equals(ExcludeMode.WhenTried)
+			|| (rplan.isPassed() && exclude.equals(ExcludeMode.WhenSucceeded))
+			|| (rplan.isFailed() && exclude.equals(ExcludeMode.WhenFailed))
+			|| (rplan.isAborted() && rplan.getException()!=null && exclude.equals(ExcludeMode.WhenFailed));
+	}
+	
 	//-------- helper methods --------
 
 	/**
@@ -681,43 +726,8 @@ public class APL
 		if(MProcessableElement.ExcludeMode.Never.equals(exclude))
 			return;
 		
-		if(((MProcessableElement)element.getModelElement()).isRebuild())
-		{
-			// Must be done after new rebuild
-			addTriedCandidate(rplan.getCandidate());
-		}
-		else
-		{
-			if(exclude.equals(MProcessableElement.ExcludeMode.WhenTried))
-			{
-				candidates.remove(rplan.getCandidate());
-			}
-			else
-			{
-	//			PlanLifecycleState state = rplan.getLifecycleState();
-				if((rplan.isPassed() && exclude.equals(MProcessableElement.ExcludeMode.WhenSucceeded))
-					|| (rplan.isFailed() && exclude.equals(MProcessableElement.ExcludeMode.WhenFailed))
-					|| (rplan.isAborted() && rplan.getException()!=null && exclude.equals(MProcessableElement.ExcludeMode.WhenFailed)))
-				{
-	//			if(state.equals(RPlan.PlanLifecycleState.PASSED)
-	//				&& exclude.equals(MProcessableElement.EXCLUDE_WHEN_SUCCEEDED)
-	//				|| (state.equals(RPlan.PlanLifecycleState.FAILED) 
-	//				&& exclude.equals(MProcessableElement.EXCLUDE_WHEN_FAILED)))
-	//			{
-					candidates.remove(rplan.getCandidate());
-				}
-			}
-		}
-	}
-	
-	/**
-	 *  Add a tried candidate. 
-	 */
-	protected void addTriedCandidate(Object candidate)
-	{
-		if(triedcandidates==null)
-			triedcandidates = new HashSet<Object>();
-		triedcandidates.add(candidate);
+		if(isToExclude(rplan, exclude))
+			candidates.remove(rplan.getCandidate());
 	}
 	
 	/** 
