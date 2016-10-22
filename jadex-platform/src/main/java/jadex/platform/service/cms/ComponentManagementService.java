8bdcda45305a672fc300b411add48174813f15b2
Alexander Pokahr <ap@activecomponents.org>
2016-10-22 18:44:40 +0200

    reuse

--- jadex-platform/src/main/java/jadex/platform/service/cms/ComponentManagementService.java
+++ jadex-platform/src/main/java/jadex/platform/service/cms/ComponentManagementService.java
@@ -1974,9 +1974,7 @@ public class ComponentManagementService implements IComponentManagementService
 				else
 				{
 					// No listener -> print exception.
-					StringWriter	sw	= new StringWriter();
-					ex.printStackTrace(new PrintWriter(sw));
-					comp.getLogger().severe("Fatal error, component '"+cid+"' will be removed.\n"+sw);
+					comp.getLogger().severe("Fatal error, component '"+cid+"' will be removed.\n"+SUtil.getExceptionStacktrace(ex));
 				}
 			}
 			
