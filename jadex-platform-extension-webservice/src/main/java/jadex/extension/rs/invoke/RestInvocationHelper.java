944e09c637afa3258e892a792fbe9b7258e0284c
kai <kj@actoron.com>
2016-10-31 15:14:33 -0500

    Post plain json.

--- jadex-platform-extension-webservice/src/main/java/jadex/extension/rs/invoke/RestInvocationHelper.java
+++ jadex-platform-extension-webservice/src/main/java/jadex/extension/rs/invoke/RestInvocationHelper.java
@@ -62,6 +62,26 @@ public class RestInvocationHelper
 										 			  final Class<?> resttype,
 										 			  final boolean inurlparams)
 	{
+		return invokeJson(component, uri, path, headers, params, null, resttype, inurlparams);
+	}
+	
+	/**
+	 *  Invokes the REST service for a JSON response.
+	 *  @param uri URI to invoke.
+	 *  @param path Path to invoke.
+	 *  @param headers Header fields.
+	 *  @param params Parameters.
+	 *  @return Reply string
+	 */
+	protected IFuture<String> invokeJson(IInternalAccess component,
+													  final String uri,
+										 			  final String path,
+										 			  final Map<String, Object> headers,
+										 			  final Map<String, Object> params,
+										 			  final String postplainjson,
+										 			  final Class<?> resttype,
+										 			  final boolean inurlparams)
+	{
 		IDaemonThreadPoolService tp = SServiceProvider.getLocalService(component.getComponentIdentifier(), IDaemonThreadPoolService.class, RequiredServiceInfo.SCOPE_PLATFORM);
 		final Future<String> ret = new Future<String>();
 		final IExternalAccess exta = component.getExternalAccess();
@@ -91,6 +111,10 @@ public class RestInvocationHelper
 								wt = wt.queryParam(entry.getKey(), entry.getValue());
 						}
 					}
+					else if (postplainjson != null)
+					{
+						data = Entity.json(postplainjson);
+					}
 					else
 					{
 						MultivaluedMap datamap = new MultivaluedHashMap();
@@ -105,7 +129,7 @@ public class RestInvocationHelper
 							else
 								datamap.put(entry.getKey(), Arrays.asList(new Object[] { entry.getValue() }));
 						}
-						Entity.form(datamap);
+						data = Entity.form(datamap);
 					}
 				}
 				
