c2c32d721e5f96084bba9eb8a371533abbcc2879
Kai Jander <kj@activecomponents.org>
2016-11-18 15:19:49 +0100

    Added missing annotation.

---
+++ jadex-kernel-bdiv3/src/main/java/jadex/bdiv3/annotation/GoalSelectCandidate.java
@@ -0,0 +1,15 @@
+package jadex.bdiv3.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *  Specify a custom method for specifying plan selection.
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GoalSelectCandidate
+{
+}
