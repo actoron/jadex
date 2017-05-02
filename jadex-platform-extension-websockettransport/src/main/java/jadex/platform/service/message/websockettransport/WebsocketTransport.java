dfc3015ace31af534f27192a450871792e2ce169
Alexander Pokahr <ap@activecomponents.org>
2017-05-02 18:35:12 +0200

    websocket transport project added

---
+++ jadex-platform-extension-websockettransport/src/main/java/jadex/platform/service/message/websockettransport/WebsocketTransport.java
@@ -0,0 +1,55 @@
+package jadex.platform.service.message.websockettransport;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.websocket.ClientEndpointConfig;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+
+import org.glassfish.tyrus.client.ClientManager;
+
+/**
+ *  Test web socket connections.
+ */
+public class WebsocketTransport
+{
+    private static CountDownLatch messageLatch;
+    private static final String SENT_MESSAGE = "Hello World";
+
+    public static void main(String [] args){
+        try {
+            messageLatch = new CountDownLatch(1);
+
+            final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
+
+            ClientManager client = ClientManager.createClient();
+            client.connectToServer(new Endpoint() {
+
+                @Override
+                public void onOpen(Session session, EndpointConfig config) {
+                    try {
+                        session.addMessageHandler(new MessageHandler.Whole<String>() {
+
+                            @Override
+                            public void onMessage(String message) {
+                                System.out.println("Received message: "+message);
+                                messageLatch.countDown();
+                            }
+                        });
+                        session.getBasicRemote().sendText(SENT_MESSAGE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, cec, new URI("ws://localhost:8025/websockets/echo"));
+            messageLatch.await(100, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
