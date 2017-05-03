52047600dc459e024267e4e09a10b6bee68d711b
Alexander Pokahr <ap@activecomponents.org>
2017-05-03 13:32:59 +0200

    small but working web socket client example

--- jadex-platform-extension-websockettransport/src/main/java/jadex/platform/service/message/websockettransport/WebsocketTransport.java
+++ jadex-platform-extension-websockettransport/src/main/java/jadex/platform/service/message/websockettransport/WebsocketTransport.java
@@ -3,7 +3,7 @@ package jadex.platform.service.message.websockettransport;
 import java.io.IOException;
 import java.net.URI;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 
 import javax.websocket.ClientEndpointConfig;
 import javax.websocket.Endpoint;
@@ -13,43 +13,54 @@ import javax.websocket.Session;
 
 import org.glassfish.tyrus.client.ClientManager;
 
+
 /**
- *  Test web socket connections.
+ * Test web socket connections.
  */
 public class WebsocketTransport
 {
-    private static CountDownLatch messageLatch;
-    private static final String SENT_MESSAGE = "Hello World";
-
-    public static void main(String [] args){
-        try {
-            messageLatch = new CountDownLatch(1);
-
-            final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
-
-            ClientManager client = ClientManager.createClient();
-            client.connectToServer(new Endpoint() {
-
-                @Override
-                public void onOpen(Session session, EndpointConfig config) {
-                    try {
-                        session.addMessageHandler(new MessageHandler.Whole<String>() {
-
-                            @Override
-                            public void onMessage(String message) {
-                                System.out.println("Received message: "+message);
-                                messageLatch.countDown();
-                            }
-                        });
-                        session.getBasicRemote().sendText(SENT_MESSAGE);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }, cec, new URI("ws://localhost:8025/websockets/echo"));
-            messageLatch.await(100, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args)
+	{
+		try
+		{
+			final CountDownLatch	latch	= new CountDownLatch(1);
+			ClientEndpointConfig	config	= ClientEndpointConfig.Builder.create().build();
+			ClientManager client = ClientManager.createClient();
+			URI	serveruri	=  new URI("ws://echo.websocket.org");
+			
+//			client.setDefaultMaxSessionIdleTimeout(...);
+			
+			Future<Session>	sesfut	= client.asyncConnectToServer(new Endpoint()
+			{
+				@Override
+				public void onOpen(Session session, EndpointConfig config)
+				{
+					session.addMessageHandler(new MessageHandler.Whole<String>()
+					{
+						@Override
+						public void onMessage(String message)
+						{
+							System.out.println("Received message: " + message);
+							latch.countDown();
+						}
+					});
+					try
+					{
+						session.getBasicRemote().sendText("xxx");
+					}
+					catch(IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}, config, serveruri);
+			
+			sesfut.get();
+			latch.await();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
