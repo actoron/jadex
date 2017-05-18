6174107f257479c5f57085d5bb5a6daddb6e45a7
Lars <lb@actoron.com>
2017-05-18 10:17:54 +0200

    fix

--- jadex-applications-micro/src/main/java/jadex/micro/testcases/visibility/Starter.java
+++ jadex-applications-micro/src/main/java/jadex/micro/testcases/visibility/Starter.java
@@ -2,8 +2,11 @@ package jadex.micro.testcases.visibility;
 
 import jadex.base.PlatformConfiguration;
 
-public class Starter {
-	public static void main(String[] args) {
+
+public class Starter
+{
+	public static void main(String[] args)
+	{
 		PlatformConfiguration config = PlatformConfiguration.getDefaultNoGui();
 		config.addComponent("jadex.micro.testcases.visibility.FIRSTAgent.class");
 		config.addComponent("jadex.micro.testcases.visibility.SECONDAgent.class");
