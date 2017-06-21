a1ee6d3369d6a8db0108c3bfcf1ccd118a4a8e8c
Alexander Pokahr <ap@activecomponents.org>
2017-06-21 17:27:46 +0200

    remote feature interface

--- jadex-bridge/src/main/java/jadex/bridge/component/IRemoteExecutionFeature.java
+++ jadex-bridge/src/main/java/jadex/bridge/component/IRemoteExecutionFeature.java
@@ -1,9 +1,18 @@
 package jadex.bridge.component;
 
+import jadex.bridge.IComponentIdentifier;
+import jadex.commons.future.IFuture;
+
 /**
  *  Feature for securely sending and handling remote execution commands.
  */
 public interface IRemoteExecutionFeature
 {
-
+	/**
+	 *  Execute a command on a remote agent.
+	 *  @param target	The component to send the command to.
+	 *  @param command	The command to be executed.
+	 *  @return	The result(s) of the command, if any.
+	 */
+	public <T> IFuture<T>	execute(IComponentIdentifier target, IRemoteCommand<T> command);
 }
