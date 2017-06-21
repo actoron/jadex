5a134474dea67bdb9b2c6ac976c9d6cee6f29430
Alexander Pokahr <ap@activecomponents.org>
2017-06-21 17:25:06 +0200

    remote command interface

---
+++ jadex-bridge/src/main/java/jadex/bridge/component/IRemoteCommand.java
@@ -0,0 +1,19 @@
+package jadex.bridge.component;
+
+import jadex.bridge.IInternalAccess;
+import jadex.bridge.service.types.security.IMsgSecurityInfos;
+import jadex.commons.future.IFuture;
+
+/**
+ *  Interface for remotely executably commands.
+ */
+public interface IRemoteCommand<T>
+{
+	/**
+	 *  Execute a command.
+	 *  @param access The agent to run the command on.
+	 *  @param secinf The established security level to decide if the command is allowed.
+	 *  @return A return value to be sent back.
+	 */
+	public IFuture<T>	execute(IInternalAccess access, IMsgSecurityInfos secinf);
+}
