50f7b597154562657046d5b5bf5c9f48258aab9c
Kai Jander <kj@actoron.com>
2017-06-28 16:47:20 +0200

    Add conversation ID.

--- jadex-bridge/src/main/java/jadex/bridge/component/IMsgHeader.java
+++ jadex-bridge/src/main/java/jadex/bridge/component/IMsgHeader.java
@@ -12,6 +12,9 @@ public interface IMsgHeader
 	/** Message header key for the receiver. */
 	public static final String RECEIVER = "receiver";
 	
+	/** Message header key for conversation IDs (optional). */
+	public static final String CONVERSATION_ID = "convid";
+	
 	/**
 	 *  Gets a property stored in the header.
 	 *  
