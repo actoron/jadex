b50558520d99c135e737a37c79a060d4b017da61
Alexander Pokahr <ap@activecomponents.org>
2017-08-10 19:16:11 +0200

    bdix messages

--- jadex-bridge/src/main/java/jadex/bridge/fipa/FipaMessage.java
+++ jadex-bridge/src/main/java/jadex/bridge/fipa/FipaMessage.java
@@ -118,10 +118,25 @@ public class FipaMessage	implements IFilter<Object>
 		this.receiver	= receiver;
 	}
 
-//	/** This parameter indicates that subsequent messages in this conversation thread
-//	 *  are to be directed to the agent named in the reply-to parameter,
-//	 *  instead of to the agent named in the sender parameter. */
-//	public IComponentIdentifier	reply_to;
+	/** This parameter indicates that subsequent messages in this conversation thread
+	 *  are to be directed to the agent named in the reply-to parameter,
+	 *  instead of to the agent named in the sender parameter. */
+	private IComponentIdentifier	reply_to;
+	/** This parameter indicates that subsequent messages in this conversation thread
+	 *  are to be directed to the agent named in the reply-to parameter,
+	 *  instead of to the agent named in the sender parameter. */
+	public IComponentIdentifier	getReplyTo()
+	{
+		return reply_to;
+	}
+	/** This parameter indicates that subsequent messages in this conversation thread
+	 *  are to be directed to the agent named in the reply-to parameter,
+	 *  instead of to the agent named in the sender parameter. */
+	public void	setReplyTo(IComponentIdentifier reply_to)
+	{
+		this.reply_to	= reply_to;
+	}
+
 
 	//-------- Content of Message -------- 
 
@@ -147,22 +162,62 @@ public class FipaMessage	implements IFilter<Object>
 		this.content	= content;
 	}
 
-//	//-------- Description of Content -------- 
-//
-//	/** Denotes the language in which the content parameter is expressed. */
-//	public String	language;
-//
-//	/** Denotes the specific encoding of the content language expression. */
-//	public String	encoding;
-//
-//	/** Denotes the ontology(s) used to give a meaning to the symbols in the content expression. */
-//	public String	ontology;
-//
-//	//-------- Control of Conversation -------- 
-//
-//	/** Denotes the interaction protocol that the sending agent is employing with this ACL message. */
-//	public String	protocol;
-//
+	//-------- Description of Content -------- 
+
+	/** Denotes the language in which the content parameter is expressed. */
+	private String	language;
+	/** Denotes the language in which the content parameter is expressed. */
+	public String	getLanguage()
+	{
+		return language;
+	}
+	/** Denotes the language in which the content parameter is expressed. */
+	public void	setLanguage(String language)
+	{
+		this.language	= language;
+	}
+
+	/** Denotes the specific encoding of the content language expression. */
+	private String	encoding;
+	/** Denotes the specific encoding of the content language expression. */
+	public String	getEncoding()
+	{
+		return encoding;
+	}
+	/** Denotes the specific encoding of the content language expression. */
+	public void	setEncoding(String encoding)
+	{
+		this.encoding	= encoding;
+	}
+
+	/** Denotes the ontology(s) used to give a meaning to the symbols in the content expression. */
+	private String	ontology;
+	/** Denotes the ontology(s) used to give a meaning to the symbols in the content expression. */
+	public String	getOntology()
+	{
+		return ontology;
+	}
+	/** Denotes the ontology(s) used to give a meaning to the symbols in the content expression. */
+	public void	setOntology(String ontology)
+	{
+		this.ontology	= ontology;
+	}
+
+	//-------- Control of Conversation -------- 
+
+	/** Denotes the interaction protocol that the sending agent is employing with this ACL message. */
+	private String	protocol;
+	/** Denotes the interaction protocol that the sending agent is employing with this ACL message. */
+	public String	getProtocol()
+	{
+		return protocol;
+	}
+	/** Denotes the interaction protocol that the sending agent is employing with this ACL message. */
+	public void	setProtocol(String protocol)
+	{
+		this.protocol	= protocol;
+	}
+
 	/** Introduces an expression (a conversation identifier) which is used to identify
 	 *  the ongoing sequence of communicative acts that together form a conversation. */
 	private String	convid;
@@ -178,16 +233,48 @@ public class FipaMessage	implements IFilter<Object>
 	{
 		this.convid	= convid;
 	}
-//
-//	/** Introduces an expression that will be used by the responding agent to identify this message. */
-//	public String	reply_with;
-//
-//	/** Denotes an expression that references an earlier action to which this message is a reply. */
-//	public String	in_reply_to;
-//
-//	/** Denotes a time and/or date expression which indicates the latest time
-//	 *  by which the sending agent would like to receive a reply. */
-//	public Long	reply_by;
+
+	/** Introduces an expression that will be used by the responding agent to identify this message. */
+	private String	reply_with;
+	/** Introduces an expression that will be used by the responding agent to identify this message. */
+	public String	getReplyWith()
+	{
+		return reply_with;
+	}
+	/** Introduces an expression that will be used by the responding agent to identify this message. */
+	public void	setReplyWith(String reply_with)
+	{
+		this.reply_with	= reply_with;
+	}
+
+	/** Denotes an expression that references an earlier action to which this message is a reply. */
+	private String	in_reply_to;
+	/** Denotes an expression that references an earlier action to which this message is a reply. */
+	public String	getInReplyTo()
+	{
+		return in_reply_to;
+	}
+	/** Denotes an expression that references an earlier action to which this message is a reply. */
+	public void	setInReplyTo(String in_reply_to)
+	{
+		this.in_reply_to	= in_reply_to;
+	}
+
+	/** Denotes a time and/or date expression which indicates the latest time
+	 *  by which the sending agent would like to receive a reply. */
+	private Long	reply_by;
+	/** Denotes a time and/or date expression which indicates the latest time
+	 *  by which the sending agent would like to receive a reply. */
+	public Long	getReplyBy()
+	{
+		return reply_by;
+	}
+	/** Denotes a time and/or date expression which indicates the latest time
+	 *  by which the sending agent would like to receive a reply. */
+	public void	setReplyBy(Long reply_by)
+	{
+		this.reply_by	= reply_by;
+	}
 	
 	//-------- methods --------
 	
