50e4f403fb72d5084505e18bc2d17279737a23e5
Lars <lb@actoron.com>
2017-09-04 17:16:03 +0200

    TransformSet 1.7

--- jadex-commons/src/main/java/jadex/commons/transformation/traverser/TransformSet.java
+++ jadex-commons/src/main/java/jadex/commons/transformation/traverser/TransformSet.java
@@ -19,132 +19,137 @@ import jadex.commons.SUtil;
  */
 public class TransformSet<E> extends HashSet<E> implements ITransformableObject
 {
-	final Collection<E> c; 
+//	final Collection<E> c; 
     final Object mutex;    
 
     public TransformSet() 
     {
-        this.c = new HashSet<E>();
+//        this.c = new HashSet<E>();
         mutex = this;
     }
     
     public TransformSet(Collection<E> c) 
     {
-        this.c = Objects.requireNonNull(c);
+//        this.c = Objects.requireNonNull(c);
         mutex = this;
     }
 
     public int size() 
     {
-        synchronized (mutex) {return c.size();}
+        synchronized (mutex) {return super.size();}
     }
     
     public boolean isEmpty() 
     {
-        synchronized (mutex) {return c.isEmpty();}
+        synchronized (mutex) {return super.isEmpty();}
     }
     
     public boolean contains(Object o) 
     {
-        synchronized (mutex) {return c.contains(o);}
+        synchronized (mutex) {return super.contains(o);}
     }
     
     public Object[] toArray() 
     {
-        synchronized (mutex) {return c.toArray();}
+        synchronized (mutex) {return super.toArray();}
     }
     
     public <T> T[] toArray(T[] a) 
     {
-        synchronized (mutex) {return c.toArray(a);}
+        synchronized (mutex) {return super.toArray(a);}
     }
 
     public Iterator<E> iterator() 
     {
-        return c.iterator(); // Must be manually synched by user!
+        return super.iterator(); // Must be manually synched by user!
     }
 
     public boolean add(E e) 
     {
-        synchronized (mutex) {return c.add(e);}
+        synchronized (mutex) {return super.add(e);}
     }
     
     public boolean remove(Object o) 
     {
-        synchronized (mutex) {return c.remove(o);}
+        synchronized (mutex) {return super.remove(o);}
     }
 
     public boolean containsAll(Collection<?> coll) 
     {
-        synchronized (mutex) {return c.containsAll(coll);}
+        synchronized (mutex) {return super.containsAll(coll);}
     }
     
     public boolean addAll(Collection<? extends E> coll) 
     {
-        synchronized (mutex) {return c.addAll(coll);}
+        synchronized (mutex) {return super.addAll(coll);}
     }
     
     public boolean removeAll(Collection<?> coll) 
     {
-        synchronized (mutex) {return c.removeAll(coll);}
+        synchronized (mutex) {return super.removeAll(coll);}
     }
     
     public boolean retainAll(Collection<?> coll) 
     {
-        synchronized (mutex) {return c.retainAll(coll);}
+        synchronized (mutex) {return super.retainAll(coll);}
     }
     
     public void clear() 
     {
-        synchronized (mutex) {c.clear();}
+        synchronized (mutex) {super.clear();}
     }
     
     public String toString() 
     {
-        synchronized (mutex) {return c.toString();}
+        synchronized (mutex) {return super.toString();}
     }
     
-    // Override default methods in Collection
-    @Override
-    public void forEach(Consumer<? super E> consumer) 
-    {
-        synchronized (mutex) {c.forEach(consumer);}
-    }
+    // todo: these methods need to be synchronized 
     
-    @Override
-    public boolean removeIf(Predicate<? super E> filter) 
-    {
-        synchronized (mutex) {return c.removeIf(filter);}
-    }
+//    // Override default methods in Collection
+//    @Override
+	  public void forEach(Consumer<? super E> consumer) 
+	  {
+		  throw new UnsupportedOperationException();
+//		  synchronized (mutex) {c.forEach(consumer);}
+	  }
+//    
+//    @Override
+	  public boolean removeIf(Predicate<? super E> filter) 
+	  {
+	  	throw new UnsupportedOperationException();
+//        synchronized (mutex) {return c.removeIf(filter);}
+	  }
     
-    @Override
-    public Spliterator<E> spliterator() 
-    {
-        return c.spliterator(); // Must be manually synched by user!
-    }
     
-    @Override
-    public Stream<E> stream() 
-    {
-        return c.stream(); // Must be manually synched by user!
-    }
-    
-    @Override
-    public Stream<E> parallelStream() 
-    {
-        return c.parallelStream(); // Must be manually synched by user!
-    }
+//    @Override
+//    public Spliterator<E> spliterator() 
+//    {
+//        return c.spliterator(); // Must be manually synched by user!
+//    }
+//    
+//    @Override
+//    public Stream<E> stream() 
+//    {
+//        return c.stream(); // Must be manually synched by user!
+//    }
+//    
+//    @Override
+//    public Stream<E> parallelStream() 
+//    {
+//        return c.parallelStream(); // Must be manually synched by user!
+//    }
     
     public boolean equals(Object o) 
     {
         if (this == o)
             return true;
-        synchronized (mutex) {return c.equals(o);}
+        synchronized (mutex) {return super.equals(o);}
     }
     
     public int hashCode() 
     {
-        synchronized (mutex) {return c.hashCode();}
+        synchronized (mutex) {return super.hashCode();}
     }
     
     private void writeObject(ObjectOutputStream s) throws IOException 
