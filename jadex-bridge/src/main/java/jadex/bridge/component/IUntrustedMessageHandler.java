ad51ca4acc078fbdc7d9ca40bb6fc0f396a143aa
Kai Jander <kj@actoron.com>
2017-09-08 14:50:48 +0200

    Added missing interface.

---
+++ jadex-bridge/src/main/java/jadex/bridge/component/IUntrustedMessageHandler.java
@@ -0,0 +1,10 @@
+package jadex.bridge.component;
+
+/**
+ *  Marker interface that allows a message handler to handle untrusted messages.
+ *
+ */
+public interface IUntrustedMessageHandler extends IMessageHandler
+{
+
+}
