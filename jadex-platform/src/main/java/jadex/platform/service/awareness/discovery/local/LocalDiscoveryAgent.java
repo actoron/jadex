a10d13058f64d31e1d2bd5c23de1700b9d662da0
Lars <lb@actoron.com>
2017-09-19 16:44:57 +0200

    local awareness

--- jadex-platform/src/main/java/jadex/platform/service/awareness/discovery/local/LocalDiscoveryAgent.java
+++ jadex-platform/src/main/java/jadex/platform/service/awareness/discovery/local/LocalDiscoveryAgent.java
@@ -1,10 +1,13 @@
 package jadex.platform.service.awareness.discovery.local;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileOutputStream;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URLEncoder;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,7 +27,6 @@ import jadex.bridge.service.types.awareness.AwarenessInfo;
 import jadex.bridge.service.types.awareness.IAwarenessManagementService;
 import jadex.bridge.service.types.awareness.IDiscoveryService;
 import jadex.bridge.service.types.threadpool.IDaemonThreadPoolService;
-import jadex.commons.Base64;
 import jadex.commons.Boolean3;
 import jadex.commons.SUtil;
 import jadex.commons.Tuple2;
@@ -43,7 +45,6 @@ import jadex.micro.annotation.RequiredServices;
 
 /**
  *  Agent providing local discovery using the file system.
- *
  */
 @Agent(autoprovide=Boolean3.TRUE)
 @Service
@@ -82,9 +83,9 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 	public IFuture<Void> start()
 	{
 		if(!DISCOVERY_DIR.exists())
-		{
 			DISCOVERY_DIR.mkdirs();
-		}
+		
+//		System.out.println("Local awareness dir: "+DISCOVERY_DIR);
 		
 		if(!(DISCOVERY_DIR.isDirectory() && DISCOVERY_DIR.canRead() && DISCOVERY_DIR.canWrite()))
 		{
@@ -93,6 +94,27 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 		}
 		else
 		{
+			try
+			{
+				final String old = DISCOVERY_DIR + File.separator+URLEncoder.encode(agent.getComponentIdentifier().getRoot().getLocalName(), "UTF-8");
+				File[] files = DISCOVERY_DIR.listFiles(new FileFilter()
+				{
+					public boolean accept(File pathname)
+					{
+						return pathname.getAbsolutePath().startsWith(old);
+					}
+				});
+				for(File file: files)
+				{
+//					System.out.println("Delete myself: "+file.getName());
+					file.delete();
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			
 			scan();
 			postInfo();
 			
@@ -101,10 +123,8 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 			{
 				public IFuture<Void> execute(IInternalAccess ia)
 				{
-					if (watchservice == null)
-					{
+					if(watchservice == null)
 						scan();
-					}
 					
 					postInfo();
 					agent.getComponentFeature(IExecutionFeature.class).waitForDelay(updaterate, this, true);
@@ -158,17 +178,25 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 						{
 							Class<?> wkclazz = Class.forName("java.nio.file.WatchKey");
 							Method polleventsmethod = wkclazz.getMethod("pollEvents", (Class<?>[]) null);
-							Method resetmethod = wkclazz.getMethod("reset", (Class<?>[]) null);
+							final Method resetmethod = wkclazz.getMethod("reset", (Class<?>[]) null);
 							Method takemethod = wsclazz.getMethod("take", (Class<?>[])null);
 							while(ex[0]==null)
 							{
 								final Object val = takemethod.invoke(watchservice, (Object[])null);
 								if (val!=null)
 								{
-									polleventsmethod.invoke(val, (Object[]) null);
-									resetmethod.invoke(val, (Object[]) null);
+									Object evs = polleventsmethod.invoke(val, (Object[]) null);
+									try{resetmethod.invoke(val, (Object[]) null);} catch(Exception e){}
+
+//									for(Object ev: SReflect.getIterable(evs))
+//									{
+//										WatchEvent we = (WatchEvent)ev;
+//										System.out.println(we.kind()+" "+we.context());
+//									}
+									
 									// Test if agent is alive in intervals and end thread otherwise
-	//								System.out.println("pollend: "+val);
+//									System.out.println("pollend: "+val);
+									
 									ea.scheduleStep(new IComponentStep<Void>()
 									{
 										public IFuture<Void> execute(IInternalAccess ia)
@@ -180,11 +208,13 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 									{
 										public void resultAvailable(Void result)
 										{
+//											try{resetmethod.invoke(val, (Object[]) null);} catch(Exception e){}
 										}
 										
 										public void exceptionOccurred(Exception exception)
 										{
 											ex[0] = exception;
+//											try{resetmethod.invoke(val, (Object[]) null);} catch(Exception e){}
 										}
 									});
 								}
@@ -209,7 +239,7 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 					}
 				});
 			}
-			catch (Exception e)
+			catch(Exception e)
 			{
 //				e.printStackTrace();
 				// Use polling as fallback.
@@ -293,8 +323,14 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 		postInfo();
 	}
 	
+	/**
+	 *  Post awareness info about myself.
+	 */
 	protected void postInfo()
 	{
+//		System.out.println("post info");
+//		Thread.dumpStack();
+		
 //		final String awa = SReflect.getInnerClassName(this.getClass());
 		final String awa = "Local";
 //		IFuture<IMessageService> fut = agent.getComponentFeature(IRequiredServicesFeature.class).getRequiredService("ms");
@@ -317,32 +353,41 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 //			}
 //		}
 //		System.out.println("=====" + agent + "======");
-		long leasetime = (Long) agent.getComponentFeature(IArgumentsResultsFeature.class).getArguments().get("leasetime");
+		long leasetime = (Long)agent.getComponentFeature(IArgumentsResultsFeature.class).getArguments().get("leasetime");
 		AwarenessInfo info = new AwarenessInfo(root, addr, AwarenessInfo.STATE_ONLINE, leasetime, null, null, null, awa);
 		byte[] data = SBinarySerializer.writeObjectToByteArray(info, agent.getClassLoader());
 		long deadline = leasetime + System.currentTimeMillis();
 		String outfilepath = DISCOVERY_DIR + File.separator; 
-		outfilepath += new String(Base64.encodeNoPadding(agent.getComponentIdentifier().getRoot().getLocalName().getBytes(SUtil.UTF8)), SUtil.UTF8);
-		outfilepath += "_" + String.valueOf(deadline) + ".awa";
-		File outfile = new File(outfilepath);
+//		outfilepath += new String(Base64.encodeNoPadding(agent.getComponentIdentifier().getRoot().getLocalName().getBytes(SUtil.UTF8)), SUtil.UTF8);
+		
 		FileOutputStream fos = null;
 		try
 		{
+			outfilepath += URLEncoder.encode(agent.getComponentIdentifier().getRoot().getLocalName(), "UTF-8");
+			outfilepath += new String();
+			outfilepath += "_" + String.valueOf(deadline) + ".awa";
+			File outfile = new File(outfilepath);
+			
 			fos = new FileOutputStream(outfile);
 			fos.write(data);
 			fos.close();
 			outfile.deleteOnExit();
 			
-			if (lastpostedfile != null)
-			{
-				lastpostedfile.delete();
-			}
-			
+			if(lastpostedfile != null)
+				try{java.nio.file.Files.delete(Paths.get(lastpostedfile.getAbsolutePath()));}catch(Exception e){e.printStackTrace();}
+//				if(!lastpostedfile.delete())
+//					System.out.println("Could not delete old file: "+lastpostedfile.getName());
+
+//			System.out.println("Created: "+outfile.getName());
+//			if(lastpostedfile!=null)
+//				System.out.println("Dele: "+lastpostedfile.getName());
+
 			lastpostedfile = outfile;
 		}
 		catch(Exception e)
 		{
-			if (fos != null)
+			e.printStackTrace();
+			if(fos != null)
 			{
 				try
 				{
@@ -362,9 +407,9 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 	{
 		File[] files = DISCOVERY_DIR.listFiles();
 //		System.out.println("FILES of " + agent + ": " + Arrays.toString(files));
-		for (File file : files)
+		for(File file : files)
 		{
-			if (file.getAbsolutePath().endsWith(".awa"))
+			if(file.getAbsolutePath().endsWith(".awa"))
 			{
 				try
 				{
@@ -373,14 +418,15 @@ public class LocalDiscoveryAgent implements IDiscoveryService
 					int index = leasetimestr.lastIndexOf('_');
 					leasetimestr = leasetimestr.substring(index + 1);
 					long leasetime = Long.parseLong(leasetimestr);
-					if (leasetime < System.currentTimeMillis())
+					if(leasetime < System.currentTimeMillis())
 					{
+//						System.out.println("Delete: "+file.getName()+" "+System.currentTimeMillis());
 						file.delete();
 					}
 					else
 					{
 						byte[] awadata = SUtil.readFile(file);
-						final AwarenessInfo awainfo = (AwarenessInfo) SBinarySerializer.readObjectFromByteArray(awadata, null, null, agent.getClassLoader(), null);
+						final AwarenessInfo awainfo = (AwarenessInfo)SBinarySerializer.readObjectFromByteArray(awadata, null, null, agent.getClassLoader(), null);
 						if(!awainfo.getSender().equals(agent.getComponentIdentifier().getRoot()))
 						{
 							IFuture<IAwarenessManagementService> msfut = agent.getComponentFeature(IRequiredServicesFeature.class).getRequiredService("management");
