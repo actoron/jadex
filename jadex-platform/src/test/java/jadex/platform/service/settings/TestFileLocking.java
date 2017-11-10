31fd8b18a558db36d5ce79c469a2afcfb73bb8d5
Alexander Pokahr <pokahr@hsu-hh.de>
2017-11-10 17:24:11 +0100

    working file locking solution

--- jadex-platform/src/test/java/jadex/platform/service/settings/TestFileLocking.java
+++ jadex-platform/src/test/java/jadex/platform/service/settings/TestFileLocking.java
@@ -1,82 +1,109 @@
 package jadex.platform.service.settings;
 
+import static org.junit.Assert.assertFalse;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  *  Check that file locking works as expected for thread safe settings writing.
  */
+// http://www.linuxtopia.org/online_books/programming_books/thinking_in_java/TIJ314_030.htm
+// http://javabeat.net/locking-files-using-java/
 public class TestFileLocking
 {
-	private static final String TESTFILE = "testfile.txt";
+	private static final File TESTFILE = new File("testfile.txt");
 
 	@Before
 	public void setup()
 	{
-		new File(TESTFILE).delete();
+		TESTFILE.delete();
 	}
 
-//	@Test
+	@Test
 	public void test() throws IOException
 	{
-		RandomAccessFile	raf1=null, raf2;
-
-		// First read access should fail.
-		try
-		{
-			raf1 = new RandomAccessFile(TESTFILE, "r");
-			Assert.fail("First read access succeeded: "+raf1);
-		}
-		catch(Exception e)
-		{
-		}
+		RandomAccessFile	raf	= null;
+		FileLock	lock	= null;
+		
+		// First access should fail.
+		assertFalse(TESTFILE+" should not exist", TESTFILE.exists());
 		
 		// First write access should be ok.
 		try
 		{
-			raf1 = new RandomAccessFile(TESTFILE, "rws");
+			raf	= new RandomAccessFile(TESTFILE, "rw");
+			lock	= raf.getChannel().lock();
 		}
 		catch(Exception e)
 		{
+			e.printStackTrace();
 			Assert.fail("First access failed: "+e);
 		}
 		
 		// Concurrent write access should fail.
 		try
 		{
-			raf2 = new RandomAccessFile(TESTFILE, "rws");
+			RandomAccessFile	raf2	= new RandomAccessFile(TESTFILE, "rw");
+			lock	= raf2.getChannel().lock();
 			Assert.fail("Concurrent write access succeeded: "+raf2);
 		}
 		catch(Exception e)
 		{
+//			e.printStackTrace();
 		}
 
-		raf1.writeBytes("Hallo\n");
+		raf.write("Hallo\n".getBytes());
 		
-		// Concurrent read access should fail.
+		// Concurrent read/write access with lock should fail.
 		try
 		{
-			raf2 = new RandomAccessFile(TESTFILE, "r");
+			RandomAccessFile	raf2	= new RandomAccessFile(TESTFILE, "rw");
+			lock	= raf.getChannel().lock();
+			raf2.read();
+			raf2.close();
+			lock.release();
 			Assert.fail("Concurrent read access succeeded: "+raf2);
 		}
 		catch(Exception e)
 		{
+//			e.printStackTrace();
 		}
 		
-		raf1.close();
+		// Concurrent read access without lock should fail.
+		try
+		{
+			RandomAccessFile	raf2	= new RandomAccessFile(TESTFILE, "r");
+			raf2.read();
+			raf2.close();
+			Assert.fail("Concurrent read access succeeded: "+raf2);
+		}
+		catch(Exception e)
+		{
+//			e.printStackTrace();
+		}
+		
+		// Todo: access from different vm?
+		
+		lock.release();
+		raf.close();
 
 		// Sequential read access should succeed.
 		try
 		{
-			raf2 = new RandomAccessFile(TESTFILE, "r");
+			RandomAccessFile	raf2	= new RandomAccessFile(TESTFILE, "r");
+			System.out.println(raf2.readLine());
 			raf2.close();
 		}
 		catch(Exception e)
 		{
+			e.printStackTrace();
 			Assert.fail("Sequential read access failed: "+e);
 		}
 	}
