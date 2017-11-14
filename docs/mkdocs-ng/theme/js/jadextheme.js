6d3765d93a1664190e38bce4771ec9f179d5096b
Julian Kalinowski <kalinowski@informatik.uni-hamburg.de>
2017-11-14 18:25:48 +0100

    styling for version in mkdocs

--- docs/mkdocs-ng/theme/js/jadextheme.js
+++ docs/mkdocs-ng/theme/js/jadextheme.js
@@ -36,7 +36,13 @@
   xdLoader.scheduleLoad['topnavbar.html'] = function (data) {
     var element = navbarLoaded2(data);
     $('#topNavBar').html(element);
-    $("#topNavBar #innertopbar").append('<div class="version-header"><div>Jadex ${jadexversiontitle}</div></div>')
+    $("#topNavBar #innertopbar").append('\
+		<div class="version-header auto-padded">\
+			<div class="version-header-container auto-scaled">\
+				<span class="version-header-jadex  auto-scaled" style="padding-right:0px; padding-left:0px">Jadex </span>\
+				<span class="auto-scaled" style="padding-left:0px; padding-right:0px">3.0.72</span>\
+			</div>\
+		</div>')
   };
 
   xdLoader.scheduleLoad['bottomnavbar.html'] = function (data) {
