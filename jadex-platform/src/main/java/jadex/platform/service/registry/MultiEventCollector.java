390d39cb77f82885f8d8c9d6cb24963f5e5451af
Lars Braubach <lb@activecomponents.org>
2017-12-11 17:09:03 +0100

    added

---
+++ jadex-platform/src/main/java/jadex/platform/service/registry/MultiEventCollector.java
@@ -0,0 +1,46 @@
+package jadex.platform.service.registry;
+
+import jadex.bridge.IComponentIdentifier;
+import jadex.bridge.service.types.registry.ARegistryEvent;
+import jadex.bridge.service.types.registry.MultiRegistryEvent;
+import jadex.commons.collection.IDelayRunner;
+
+/**
+ *  Collector for multiple registry events.
+ */
+public abstract class MultiEventCollector extends EventCollector
+{
+	/**
+	 *  Create a new registry observer.
+	 */
+	public MultiEventCollector(IComponentIdentifier cid, final IDelayRunner timer)
+	{
+		this(cid, timer, 50, 10000);
+	}
+	
+	/**
+	 *  Create a new registry observer.
+	 */
+	public MultiEventCollector(IComponentIdentifier cid, final IDelayRunner timer, int eventslimit, final long timelimit)
+	{
+		super(cid, timer, eventslimit, timelimit);
+	}
+
+	/**
+	 *  Add a registry event.
+	 *  @param event The event.
+	 */
+	public void addEvent(ARegistryEvent event)
+	{
+		((MultiRegistryEvent)event).addEvent(event);
+	}
+	
+	/**
+	 *  Create an event.
+	 *  @return The event.
+	 */
+	public ARegistryEvent createEvent()
+	{
+		return new MultiRegistryEvent(eventslimit, timelimit);
+	}
+}
