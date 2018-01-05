21964247b1ce5dbe073704ed670851988a4d585f
Kai Jander <kj@actoron.com>
2018-01-05 12:33:30 +0100

    Print platform name along with the platform secret.

--- jadex-platform/src/main/java/jadex/platform/service/security/SecurityAgent.java
+++ jadex-platform/src/main/java/jadex/platform/service/security/SecurityAgent.java
@@ -276,15 +276,16 @@ public class SecurityAgent implements ISecurityService, IInternalService
 		if (printsecret && platformsecret != null)
 		{
 			secretstr = platformsecret.toString();
+			String pfname = agent.getComponentIdentifier().getPlatformName();
 			
 			if (platformsecret instanceof PasswordSecret)
-				System.out.println("Platform access password: "+secretstr);
+				System.out.println("Platform " + pfname + " access password: "+secretstr);
 			else if (platformsecret instanceof KeySecret)
-				System.out.println("Platform access key: "+secretstr);
+				System.out.println("Platform " + pfname + " access key: "+secretstr);
 			else if (platformsecret instanceof AbstractX509PemSecret)
-				System.out.println("Platform access certificates: "+secretstr);
+				System.out.println("Platform " + pfname + " access certificates: "+secretstr);
 			else
-				System.out.println("Platform access secret: "+secretstr);
+				System.out.println("Platform " + pfname + " access secret: "+secretstr);
 		}
 		
 		initializingcryptosuites = new HashMap<String, HandshakeState>();
