35a59372bcce659eceb14ef129961c852a43dd99
Alexander Pokahr <ap@actoron.com>
2018-02-09 23:32:35 +0100

    build fixes / performance improvements

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 @REM perform a clean build and test
 
 @REM --- with recompile ---
-gradlew --parallel clean compileJava compileTestJava compileReleaseJavaWithJavac compileDebugUnitTestJavaWithJavac test -x :jadex-integration-performance-test:test -x javadoc -x lint -x generatereleaseJavadoc -x generatedebugJavadoc -x lintVitalRelease -x transformClassesWithDexForRelease -x testDebugUnitTest -x processSchemas
+gradlew --parallel --profile clean test -x :jadex-integration-performance-test:test -x javadoc -x lint -x generatereleaseJavadoc -x generatedebugJavadoc -x lintVitalRelease -x transformClassesWithDexForRelease -x testDebugUnitTest -x processSchemas
