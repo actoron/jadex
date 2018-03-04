5345879608a6fd2b1c5ca1fc50b1d72988bc8d3d
Alexander Pokahr <pokahr@hsu-hh.de>
2018-03-04 10:46:08 +0100

    cleanup quickstart

--- jadex-applications-micro/src/main/java/jadex/micro/quickstart/TimeUserAgent.java
+++ jadex-applications-micro/src/main/java/jadex/micro/quickstart/TimeUserAgent.java
@@ -6,30 +6,26 @@ import jadex.base.Starter;
 import jadex.bridge.service.IService;
 import jadex.commons.future.ISubscriptionIntermediateFuture;
 import jadex.micro.annotation.Agent;
-import jadex.micro.annotation.AgentServiceSearch;
+import jadex.micro.annotation.AgentServiceQuery;
 import jadex.micro.annotation.Binding;
-import jadex.micro.annotation.RequiredService;
-import jadex.micro.annotation.RequiredServices;
 
 /**
  *  Simple agent that uses globally available time services.
  */
 @Agent
-@RequiredServices(@RequiredService(name="timeservices", type=ITimeService.class, multiple=true,
-	binding=@Binding(scope=Binding.SCOPE_GLOBAL)))
 public class TimeUserAgent
 {
 	/**
-	 *  The time services are searched and added at agent startup.
+	 *  The time services are searched and added whenever a new one is found.
 	 */
-	@AgentServiceSearch//(isquery=true)
+	@AgentServiceQuery(scope=Binding.SCOPE_GLOBAL)
 	public void	addTimeService(ITimeService timeservice)
 	{
-		ISubscriptionIntermediateFuture<String>	subscription	= timeservice.subscribe();
+		ISubscriptionIntermediateFuture<String>	subscription = timeservice.subscribe();
 		while(subscription.hasNextIntermediateResult())
 		{
-			String	time	= subscription.getNextIntermediateResult();
-			String	platform	= ((IService)timeservice).getServiceIdentifier().getProviderId().getPlatformName();
+			String time = subscription.getNextIntermediateResult();
+			String platform	= ((IService)timeservice).getServiceIdentifier().getProviderId().getPlatformName();
 			System.out.println("New time received from "+platform+" at "+timeservice.getLocation()+": "+time);
 		}
 	}
@@ -37,10 +33,11 @@ public class TimeUserAgent
 	/**
 	 *  Start a Jadex platform and the TimeUserAgent.
 	 */
-	public static void	main(String[] args)
+	public static void main(String[] args)
 	{
-		IPlatformConfiguration	config	= PlatformConfigurationHandler.getMinimalRelayAwareness();
-		config.addComponent(TimeUserAgent.class.getName()+".class");
+		IPlatformConfiguration config = PlatformConfigurationHandler.getDefaultNoGui();
+		config.addComponent(TimeUserAgent.class);
 		Starter.createPlatform(config).get();
 	}
 }
+
