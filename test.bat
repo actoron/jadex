5650fd5b3295471ac437df0cf430d90b73b719e4
Alexander Pokahr <pokahr@hsu-hh.de>
2018-03-05 16:14:56 +0100

    continue after failure to execute remaining tests too

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 @REM perform a clean build and test
 
 @REM --- with recompile ---
-gradlew --parallel clean :jadex-applications-micro:test :jadex-integration-test:test test testReport -x :jadex-integration-performance-test:test -x javadoc -x lint -x generatereleaseJavadoc -x generatedebugJavadoc -x lintVitalRelease -x transformClassesWithDexForRelease -x testDebugUnitTest -x processSchemas
+gradlew --parallel  --continue clean :jadex-applications-micro:test :jadex-integration-test:test test testReport -x :jadex-integration-performance-test:test -x javadoc -x lint -x generatereleaseJavadoc -x generatedebugJavadoc -x lintVitalRelease -x transformClassesWithDexForRelease -x testDebugUnitTest -x processSchemas
