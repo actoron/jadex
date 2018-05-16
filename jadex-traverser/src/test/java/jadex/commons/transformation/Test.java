1dd7d2dea197a7fe7372d2819c2e9c8845031385
Kai Jander <kj@actoron.com>
2018-05-16 12:33:01 +0200

    More Android fixes.

--- jadex-traverser/src/test/java/jadex/commons/transformation/Test.java
+++ jadex-traverser/src/test/java/jadex/commons/transformation/Test.java
@@ -5,21 +5,14 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URL;
-import java.security.GeneralSecurityException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.KeyStore;
-import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -37,10 +30,6 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
-
-import javax.security.auth.x500.X500Principal;
-
-import org.bouncycastle.x509.X509V1CertificateGenerator;
 
 import jadex.bridge.ClassInfo;
 import jadex.commons.ErrorException;
@@ -1256,7 +1245,10 @@ public abstract class Test extends TestCase
 	 */
 	public void	testCertificate() throws Exception
 	{
-		KeyStore ks = getKeystore("./testkeystore", "pass", "pass", "test");
+		
+		//KeyStore ks = getKeystore("./testkeystore", "pass", "pass", "test");
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(new ByteArrayInputStream(TestKeyStore.KEYSTORE_DATA), "pass".toCharArray());
 		Certificate cert = ks.getCertificate("test");
 		doWriteAndRead(cert, new Comparator<Certificate>()
 		{
@@ -1452,142 +1444,4 @@ public abstract class Test extends TestCase
 			return true;
 		}
 	}
-
-	// copied from SSecurity to avoid dependencies :-( 
-	
-	/**
-	 *  Get keystore from a given file.
-	 */
-	public static KeyStore getKeystore(String storepath, String storepass, String keypass, String alias)
-	{
-		try
-		{
-			KeyStore ks = KeyStore.getInstance("JKS");
-			FileInputStream fis = null;
-			boolean loaded = false;
-			try
-			{
-				File f = new File(storepath);
-				if(f.exists())
-				{
-					fis = new FileInputStream(storepath);
-					ks.load(fis, storepass.toCharArray());
-					loaded = true;
-				}
-			}
-			catch(Exception e)
-			{
-			}
-			finally
-			{
-				if(fis!=null)
-					fis.close();
-				if(!loaded || !ks.containsAlias(alias))
-					initKeystore(ks, storepath, storepass, keypass, alias);
-			}
-			return ks;
-		}
-		catch(Exception e)
-		{
-//			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
-	
-	/**
-	 *  Get keystore from a given file.
-	 */
-	public static void saveKeystore(KeyStore keystore, String storepath, String storepass)
-	{
-		FileOutputStream fos = null;
-		try
-		{
-			fos = new FileOutputStream(storepath);
-			keystore.store(fos, storepass.toCharArray());
-		}
-		catch(Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-		finally
-		{
-			if(fos!=null)
-			{
-				try
-				{
-					fos.close();
-				}
-				catch(Exception e)
-				{
-				}
-			}
-		}
-	}
-	
-	/**
-	 *  Init keystore with a self-signed certificate.
-	 */
-	public static void initKeystore(KeyStore ks, String storepath, String storepass, String keypass, String alias)
-	{
-		try
-    	{
-	    	ks.load(null, null); // Must be called. 
-	    	
-//	    	RSAKeyPairGenerator r = new RSAKeyPairGenerator();
-//	    	r.init(new KeyGenerationParameters(new SecureRandom(), 1024));
-//	    	AsymmetricCipherKeyPair keys = r.generateKeyPair();
-	    	
-	    	KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");  
-	 	    gen.initialize(1024);  
-	 	    
-//	 	    System.out.println("Generating key pair, this may take a short while.");
-	 	    KeyPair keys = gen.generateKeyPair();
-//	 	    System.out.println("Key generation finished.");
-	 	    
-		    Certificate c = generateCertificate("CN=CKS Self Signed Cert", keys, 1000, "MD5WithRSA");
-		    
-		    ks.setKeyEntry(alias, keys.getPrivate(), keypass.toCharArray(),  
-		    	new java.security.cert.Certificate[]{c});  
-		    
-		    saveKeystore(ks, storepath, storepass);
-    	}
-		catch(RuntimeException e)
-		{
-			throw e;
-		}
-		catch(Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
-	
-	/** 
-	 * Create a self-signed X.509 Certificate
-	 * @param dn the X.509 Distinguished Name, eg "CN=Test, L=London, C=GB"
-	 * @param pair the KeyPair
-	 * @param days how many days from now the Certificate is valid for
-	 * @param algorithm the signing algorithm, eg "SHA1withRSA"
-	 */ 
-	public static Certificate generateCertificate(String dn, KeyPair pair, int days, String algorithm) throws GeneralSecurityException, IOException
-	{
-		X509V1CertificateGenerator gen = new X509V1CertificateGenerator();
-		X500Principal dnn = new X500Principal(dn); //"CN=Test CA Certificate"
-
-		Date from = new Date();
-		Date to = new Date(from.getTime() + days * 86400000l);
-		BigInteger sn = new BigInteger(64, new SecureRandom());
-		
-		gen.setSerialNumber(sn);
-		gen.setIssuerDN(dnn);
-		gen.setNotBefore(from);
-		gen.setNotAfter(to);
-		gen.setSubjectDN(dnn);                       // note: same as issuer
-		gen.setPublicKey(pair.getPublic());
-		gen.setSignatureAlgorithm(algorithm);
-
-		Certificate cert = gen.generate(pair.getPrivate());
-		
-		return cert;
-	}   
-	
 }
