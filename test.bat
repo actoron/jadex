28a3e0b0f3b86221a104a769e65182e443b04a60
Alexander Pokahr <ap@actoron.com>
2018-05-17 13:57:23 +0200

    fix test bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @REM perform a clean build and test
-CMD /C gradlew --parallel  --continue clean :jadex-applications-micro:test :jadex-integration-test:test test testReport -x :jadex-integration-performance-test:test -x javadoc -x lint -x generatereleaseJavadoc -x lintVitalRelease -x transformClassesWithDexForRelease -x processSchemas
+CMD /C gradlew --parallel  --continue clean :jadex-applications-micro:test :jadex-integration-test:test test testReport -x :jadex-integration-performance-test:test -x javadoc -x lint -x generatereleaseJavadoc -x lintVitalRelease -x processSchemas
 @set builderror=%ERRORLEVEL%
 @echo Generating Test Report...
 @CMD /C gradlew testReport >NUL
