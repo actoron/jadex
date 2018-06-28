b2b5b55a5d2fc6607a90f159e2710fbf12fce60a
Alexander Pokahr <ap@actoron.com>
2018-06-28 14:21:01 +0200

    fix

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @REM perform a clean distribution build
-gradlew --profile -Pdist=publishdists clean test distZips -x javadoc -x lint -x generatereleaseJavadoc -x lintVitalRelease
+gradlew --profile -Pdist=publishdists clean test distZips -x javadoc -x lint -x lintVitalRelease
 @rem  --parallel --continue
