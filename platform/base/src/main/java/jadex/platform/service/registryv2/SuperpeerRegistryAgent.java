36d87d9517c55248dacc9c866a948a9c8efbfa60
Alex <ap@actoron.com>
2018-07-01 13:06:54 +0200

    started future command registry sync impl

--- platform/base/src/main/java/jadex/platform/service/registryv2/SuperpeerRegistryAgent.java
+++ platform/base/src/main/java/jadex/platform/service/registryv2/SuperpeerRegistryAgent.java
@@ -1,11 +1,14 @@
 package jadex.platform.service.registryv2;
 
+import jadex.bridge.SFuture;
 import jadex.bridge.service.annotation.Service;
 import jadex.bridge.service.search.ServiceQuery;
-import jadex.bridge.service.types.registryv2.ISuperpeerClientService;
 import jadex.bridge.service.types.registryv2.ISuperpeerService;
 import jadex.commons.future.IFuture;
 import jadex.commons.future.ISubscriptionIntermediateFuture;
+import jadex.commons.future.ITerminableFuture;
+import jadex.commons.future.TerminableFuture;
+import jadex.commons.future.TerminationCommand;
 import jadex.micro.annotation.Agent;
 import jadex.micro.annotation.Binding;
 import jadex.micro.annotation.ProvidedService;
@@ -23,23 +26,36 @@ public class SuperpeerRegistryAgent	extends RemoteRegistryAgent	implements ISupe
 {
 	/**
 	 *  Initiates the client registration procedure
-	 *  (super peer will perform a subscription callback).
+	 *  (super peer will answer initially with a forward command,
+	 *  client will send updates with backward commands).
 	 *  
-	 *  @param networkname	Network name for callback. 
-	 *  @param client	Subscription service for callback. 
+	 *  @param networkname	Network for this connection. 
 	 *  
-	 *  @return Null, when registration is complete.
+	 *  @return Does not return while connection is running.
 	 */
-	public IFuture<Void> registerClient(String networkname, ISuperpeerClientService client)
+	// TODO: replace internal commands with typed channel (i.e. bidirectional / reverse subscription future)
+	// TODO: network name required for server?
+	public ITerminableFuture<Void> registerClient(String networkname)
 	{
-//		ISubscriptionIntermediateFuture<ServiceEvent<IServiceIdentifier>>	fut	=
-			client.addSuperpeerSubscription(networkname, this);
+		TerminableFuture<Void>	ret	= new TerminableFuture<>(new TerminationCommand()
+		{
+			@Override
+			public void terminated(Exception reason)
+			{
+				System.out.println("Super peer connection terminated: "+reason);
+			}
+		});
 		
+		SFuture.avoidCallTimeouts(ret, ia);
+		
+		// Initial register-ok response
+		ret.sendForwardCommand(null);
+
 		// TODO: listen for changes and add new services locally.
 
 		// TODO: when connection is lost, remove all services and queries from client.
 		
-		return IFuture.DONE;
+		return ret;
 	}
 	
 	/**
--- platform/bridge/src/main/java/jadex/bridge/service/types/registryv2/ISuperpeerService.java
+++ platform/bridge/src/main/java/jadex/bridge/service/types/registryv2/ISuperpeerService.java
@@ -4,6 +4,7 @@ import jadex.bridge.service.annotation.Service;
 import jadex.bridge.service.search.ServiceQuery;
 import jadex.commons.future.IFuture;
 import jadex.commons.future.ISubscriptionIntermediateFuture;
+import jadex.commons.future.ITerminableFuture;
 
 /**
  *  Service for service registry superpeers.
@@ -14,15 +15,16 @@ public interface ISuperpeerService extends IRemoteRegistryService
 {
 	/**
 	 *  Initiates the client registration procedure
-	 *  (super peer will perform a subscription callback).
+	 *  (super peer will answer initially with a forward command,
+	 *  client will send updates with backward commands).
 	 *  
-	 *  @param networkname	Network name for callback. 
-	 *  @param client	Subscription service for callback. 
+	 *  @param networkname	Network for this connection. 
 	 *  
-	 *  @return Null, when registration is complete.
+	 *  @return Does not return while connection is running.
 	 */
-	// TODO: replace ping pong with Channel (i.e. bidirectional subscription future)
-	public IFuture<Void> registerClient(String networkname, ISuperpeerClientService client);
+	// TODO: replace internal commands with typed channel (i.e. bidirectional / reverse subscription future)
+	// TODO: network name required for server?
+	public ITerminableFuture<Void> registerClient(String networkname);
 	
 	/**
 	 *  Add a service query to the registry.
