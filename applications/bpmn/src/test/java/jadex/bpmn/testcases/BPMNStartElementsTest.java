f95bcdba693e9fde62a3a5c16310e4fced38f4fe
Alex <ap@actoron.com>
2018-07-16 00:34:34 +0200

    cleanup

--- applications/bpmn/src/test/java/jadex/bpmn/testcases/BPMNStartElementsTest.java
+++ applications/bpmn/src/test/java/jadex/bpmn/testcases/BPMNStartElementsTest.java
@@ -20,25 +20,26 @@ public class BPMNStartElementsTest //extends TestCase
 	{
 //		System.err.println("starting platform");
 		String projectroot = new String("jadex-applications-bpmn");
-		IFuture<IExternalAccess>	fut	= Starter.createPlatform(STest.getDefaultTestConfig(), new String[]{"-platformname", "testcases_*",
-//				"-kernels", "\"all\"",	// Required for old hudson build, otherwise wrong bdi kernel is used as dependencies are not in correct order
-				"-simulation", "true",
-				"-asyncexecution", "true",
-				"-libpath", SUtil.getOutputDirsExpression(projectroot, true),
-//				"-logging", "true", // path.toString().indexOf("bdibpmn")!=-1 ? "true" : "false",
-				"-logging_level", "java.util.logging.Level.WARNING",
-//				"-debugfutures", "true",
-//				"-nostackcompaction", "true",
-				"-gui", "false",
-				"-awareness", "false",
-				"-saveonexit", "false",
-				"-welcome", "false",
-				"-autoshutdown", "false",
-				"-opengl", "false",
-				"-cli", "false",
-				"-superpeerclient", "false", // TODO: fails on shutdown due to auto restart
-//				"-deftimeout", "-1",
-				"-printpass", "false",});
+		IFuture<IExternalAccess>	fut	= Starter.createPlatform(STest.getDefaultTestConfig());
+//			, new String[]{"-platformname", "testcases_*",
+////				"-kernels", "\"all\"",	// Required for old hudson build, otherwise wrong bdi kernel is used as dependencies are not in correct order
+//				"-simulation", "true",
+//				"-asyncexecution", "true",
+//				"-libpath", SUtil.getOutputDirsExpression(projectroot, true),
+////				"-logging", "true", // path.toString().indexOf("bdibpmn")!=-1 ? "true" : "false",
+//				"-logging_level", "java.util.logging.Level.WARNING",
+////				"-debugfutures", "true",
+////				"-nostackcompaction", "true",
+//				"-gui", "false",
+//				"-awareness", "false",
+//				"-saveonexit", "false",
+//				"-welcome", "false",
+//				"-autoshutdown", "false",
+//				"-opengl", "false",
+//				"-cli", "false",
+//				"-superpeerclient", "false", // TODO: fails on shutdown due to auto restart
+////				"-deftimeout", "-1",
+//				"-printpass", "false",});
 		
 		long timeout = Starter.getLocalDefaultTimeout(null);
 		
