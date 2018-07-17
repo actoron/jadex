b04b9c80345c9dd929dc8f4328b97c92a474b07b
Alexander Pokahr <ap@actoron.com>
2018-07-17 13:59:44 +0200

    cleanup

--- applications/bpmn/src/test/java/jadex/bpmn/testcases/BPMNStartElementsTest.java
+++ applications/bpmn/src/test/java/jadex/bpmn/testcases/BPMNStartElementsTest.java
@@ -4,14 +4,17 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import jadex.base.IPlatformConfiguration;
 import jadex.base.Starter;
 import jadex.base.test.util.STest;
+import jadex.bridge.IComponentIdentifier;
 import jadex.bridge.IExternalAccess;
 import jadex.bridge.service.search.ServiceQuery;
 import jadex.bridge.service.types.cms.CreationInfo;
 import jadex.bridge.service.types.cms.IComponentManagementService;
-import jadex.commons.SUtil;
+import jadex.commons.future.Future;
 import jadex.commons.future.IFuture;
+import jadex.commons.future.ITuple2Future;
 
 public class BPMNStartElementsTest //extends TestCase
 {
@@ -19,27 +22,13 @@ public class BPMNStartElementsTest //extends TestCase
 	public void testStartActivities()
 	{
 //		System.err.println("starting platform");
-		String projectroot = new String("jadex-applications-bpmn");
-		IFuture<IExternalAccess>	fut	= Starter.createPlatform(STest.getDefaultTestConfig());
-//			, new String[]{"-platformname", "testcases_*",
-////				"-kernels", "\"all\"",	// Required for old hudson build, otherwise wrong bdi kernel is used as dependencies are not in correct order
-//				"-simulation", "true",
-//				"-asyncexecution", "true",
-//				"-libpath", SUtil.getOutputDirsExpression(projectroot, true),
-////				"-logging", "true", // path.toString().indexOf("bdibpmn")!=-1 ? "true" : "false",
-//				"-logging_level", "java.util.logging.Level.WARNING",
-////				"-debugfutures", "true",
-////				"-nostackcompaction", "true",
-//				"-gui", "false",
-//				"-awareness", "false",
-//				"-saveonexit", "false",
-//				"-welcome", "false",
-//				"-autoshutdown", "false",
-//				"-opengl", "false",
-//				"-cli", "false",
-//				"-superpeerclient", "false", // TODO: fails on shutdown due to auto restart
-////				"-deftimeout", "-1",
-//				"-printpass", "false",});
+		IPlatformConfiguration	config	= STest.getDefaultTestConfig();
+//		config.setGui(true);
+//		config.setDefaultTimeout(-1);
+//		config.setValue("kernel_multi", false);
+//		config.setValue("kernel_micro", true);
+//		config.setValue("kernel_bpmn", true);
+		IFuture<IExternalAccess>	fut	= Starter.createPlatform(config);
 		
 		long timeout = Starter.getLocalDefaultTimeout(null);
 		
@@ -52,7 +41,11 @@ public class BPMNStartElementsTest //extends TestCase
 		CreationInfo ci = new CreationInfo();
 		ci.setConfiguration("Case A");
 		
-		Map<String, Object> results = cms.createComponent("jadex.bpmn.testcases.StartElements.bpmn2", ci).getSecondResult();
+		ITuple2Future<IComponentIdentifier, Map<String, Object>>	fut2	= cms.createComponent("jadex.bpmn.testcases.StartElements.bpmn2", ci);
+		
+//		new Future<>().get(-1);
+		
+		Map<String, Object> results = fut2.getSecondResult();
 		if (!("A".equals(results.get("result"))))
 		{
 			throw new RuntimeException("BPMN start elements tests: Results do not match, expected A, got " + results.get("result") + ".");
