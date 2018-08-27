4e13e908ec858352e75daf09ffb3d5ed94a7155f
Lars <lb@actoron.com>
2018-08-27 15:27:34 +0200

    status event

---
+++ applications/micro/src/main/java/jadex/micro/examples/constructorstart/StarterAgent.java
@@ -0,0 +1,41 @@
+package jadex.micro.examples.constructorstart;
+
+import jadex.base.Starter;
+import jadex.bridge.IExternalAccess;
+import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.AgentBody;
+
+/**
+ * 
+ */
+@Agent
+public class StarterAgent
+{
+	/**
+	 * 
+	 */
+	public StarterAgent()
+	{
+		System.out.println("my hashcode: "+this.hashCode());
+	}
+
+	/**
+	 *  The agent body.
+	 */
+	@AgentBody
+	public void body()
+	{
+		System.out.println("body");
+		System.out.println("my hashcode: "+this.hashCode());
+	}
+	
+	/**
+	 *  Main for testing.
+	 *  @param args The arguments.
+	 */
+	public static void main(String[] args)
+	{
+		IExternalAccess platform = Starter.createPlatform().get();
+		IExternalAccess agent = platform.createComponent(new StarterAgent(), null, null).get();
+	}
+}
---
+++ platform/bridge/src/main/java/jadex/bridge/service/types/cms/CMSStatusEvent.java
@@ -0,0 +1,194 @@
+package jadex.bridge.service.types.cms;
+
+import java.util.Map;
+
+import jadex.bridge.IComponentIdentifier;
+import jadex.commons.SReflect;
+
+/**
+ *  Base change event. If used w/o subclass denotes change in description. 
+ */
+public class CMSStatusEvent
+{
+	/** The component description. */
+	protected IComponentDescription desc;
+	
+	/**
+	 *  Create a new CMSStatusEvent.
+	 */
+	public CMSStatusEvent()
+	{
+	}
+	
+	/**
+	 *  Create a new CMSStatusEvent.
+	 */
+	public CMSStatusEvent(IComponentDescription desc)
+	{
+		this.desc = desc;
+	}
+
+	/**
+	 *  Get the componentIdentifier.
+	 *  @return The componentIdentifier.
+	 */
+	public IComponentIdentifier getComponentIdentifier()
+	{
+		return desc.getName();
+	}
+
+	/**
+	 *  Get the component description.
+	 *  @return The description.
+	 */
+	public IComponentDescription getComponentDescription()
+	{
+		return desc;
+	}
+
+	/**
+	 *  Set the component description.
+	 *  @param desc The component description to set.
+	 */
+	public void setComponentDescription(IComponentDescription desc)
+	{
+		this.desc = desc;
+	}
+
+	/**
+	 *  Get the string representation.
+	 */
+	public String toString()
+	{
+		return SReflect.getInnerClassName(getClass())+"["+desc+"]";
+	}
+	
+	/**
+	 *  Status event for a newly created component.
+	 */
+	public static class CMSCreatedEvent extends CMSStatusEvent
+	{
+		/**
+		 *  Create a new CMSCreatedEvent. 
+		 */
+		public CMSCreatedEvent()
+		{
+		}
+		
+		/**
+		 *  Create a new CMSCreatedEvent. 
+		 */
+		public CMSCreatedEvent(IComponentDescription desc)
+		{
+			super(desc);
+		}
+	}
+	
+	/**
+	 *  Status event for an intermediate result of a component.
+	 */
+	public static class CMSIntermediateResultEvent extends CMSStatusEvent
+	{
+		/** The name of the result. */
+		protected String name;
+		
+		/** The value of the result. */
+		protected Object value;
+
+		/**
+		 *  Create a new CMSIntermediateResultEvent. 
+		 */
+		public CMSIntermediateResultEvent()
+		{
+		}
+		
+		/**
+		 *  Create a new CMSIntermediateResultEvent. 
+		 */
+		public CMSIntermediateResultEvent(IComponentDescription desc, String name, Object value)
+		{
+			super(desc);
+			this.name = name;
+			this.value = value;
+		}
+
+		/**
+		 *  Get the name.
+		 *  @return The name.
+		 */
+		public String getName()
+		{
+			return name;
+		}
+
+		/**
+		 *  Set the name.
+		 *  @param name The name to set.
+		 */
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+
+		/**
+		 *  Get the value.
+		 *  @return The value.
+		 */
+		public Object getValue()
+		{
+			return value;
+		}
+
+		/**
+		 *  Set the value.
+		 *  @param value The value to set.
+		 */
+		public void setValue(Object value)
+		{
+			this.value = value;
+		}
+	}
+	
+	/**
+	 *  Final event of a finished component, including all results. 
+	 */
+	public static class CMSTerminatedEvent extends CMSStatusEvent
+	{
+		/** The cid. */
+		protected Map<String, Object> results;
+
+		/**
+		 *  Create a new CMSCreatedEvent. 
+		 */
+		public CMSTerminatedEvent()
+		{
+		}
+		
+		/**
+		 *  Create a new CMSCreatedEvent. 
+		 */
+		public CMSTerminatedEvent(IComponentDescription desc, Map<String, Object> results)
+		{
+			super(desc);
+			this.results = results;
+		}
+
+		/**
+		 *  Get the results.
+		 *  @return The results.
+		 */
+		public Map<String, Object> getResults()
+		{
+			return results;
+		}
+
+		/**
+		 *  Set the results.
+		 *  @param results The results to set.
+		 */
+		public void setResults(Map<String, Object> results)
+		{
+			this.results = results;
+		}
+	}
+}
