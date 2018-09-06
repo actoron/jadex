5d828dfce2a4ef0a142334147cb1ae615362b26a
Lars <lb@actoron.com>
2018-09-06 11:59:11 +0200

    fix

---
+++ platform/bridge/src/main/java/jadex/bridge/component/IExternalComponentFeature.java
@@ -0,0 +1,10 @@
+package jadex.bridge.component;
+
+/**
+ *  Marker interface for external component feature interfaces.
+ *  
+ *  Is used in getExternalFeature() to check if the interface is valid.
+ */
+public interface IExternalComponentFeature
+{
+}
