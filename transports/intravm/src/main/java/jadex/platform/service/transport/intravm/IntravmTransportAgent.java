7c6d6f2ee25ac28e03b331c868431a19fa6c2663
Alex <ap@actoron.com>
2018-09-06 02:36:01 +0200

    fix + enable intravm transport for testing

--- transports/intravm/src/main/java/jadex/platform/service/transport/intravm/IntravmTransportAgent.java
+++ transports/intravm/src/main/java/jadex/platform/service/transport/intravm/IntravmTransportAgent.java
@@ -3,14 +3,14 @@ package jadex.platform.service.transport.intravm;
 import jadex.commons.Boolean3;
 import jadex.micro.annotation.Agent;
 import jadex.micro.annotation.Autostart;
-import jadex.platform.service.transport.AbstractTransportAgent2;
+import jadex.platform.service.transport.AbstractTransportAgent;
 import jadex.platform.service.transport.ITransport;
 
 /**
  *  Agent that implements TCP message transport.
  */
 @Agent(autostart=@Autostart(value=Boolean3.FALSE, name="intravm", predecessors="jadex.platform.service.address.TransportAddressAgent"))
-public class IntravmTransportAgent extends AbstractTransportAgent2<IntravmTransport>
+public class IntravmTransportAgent extends AbstractTransportAgent<IntravmTransport>
 {
 	//-------- abstract methods to be provided by concrete transport --------
 	
