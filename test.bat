101f04620b19214987389659ae0f3622a8f30022
Alex <ap@actoron.com>
2018-09-10 22:30:54 +0200

    test.bat

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 @REM perform a clean build and test
 @rem set jadex_timeout=90000
 
-CMD /C gradlew -Pdist=publishdists :applications:micro:test test -x javadoc -x processSchemas --continue
+CMD /C gradlew -Pdist=publishdists cleanTest :applications:micro:test test -x javadoc -x processSchemas --continue
 
 @set builderror=%ERRORLEVEL%
 @IF /I "%builderror%" NEQ "0" (@echo Generating Test Report... & @CMD /C gradlew testReport >NUL & START "" "testreport\index.html")
