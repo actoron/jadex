cd641b1977dfad7af9843dbf4bd35b6c26cae72a
Alexander Pokahr <pokahr@hsu-hh.de>
2018-10-15 17:39:19 +0200

    exercise c started

--- applications/quickstart/src/main/java/jadex/quickstart/cleanerworld/Main.java
+++ applications/quickstart/src/main/java/jadex/quickstart/cleanerworld/Main.java
@@ -38,7 +38,7 @@ public class Main
 		conf.setValue("kernel_bdi", true);
         
         // Add your cleaner agent(s)
-		conf.addComponent("jadex/quickstart/cleanerworld/single/CleanerBDIAgentB3.class");
+		conf.addComponent("jadex/quickstart/cleanerworld/single/CleanerBDIAgent.class");
 
 		// Start a Jadex platform (asynchronously in background).
 		IFuture<IExternalAccess>	fut	= Starter.createPlatform(conf);
--- applications/quickstart/src/main/java/jadex/quickstart/cleanerworld/single/CleanerBDIAgent.java
+++ applications/quickstart/src/main/java/jadex/quickstart/cleanerworld/single/CleanerBDIAgent.java
@@ -1,5 +1,8 @@
 package jadex.quickstart.cleanerworld.single;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jadex.bdiv3.annotation.Belief;
 import jadex.bdiv3.annotation.Deliberation;
 import jadex.bdiv3.annotation.Goal;
@@ -8,7 +11,6 @@ import jadex.bdiv3.annotation.GoalTargetCondition;
 import jadex.bdiv3.annotation.Plan;
 import jadex.bdiv3.annotation.Trigger;
 import jadex.bdiv3.features.IBDIAgentFeature;
-import jadex.bdiv3.runtime.impl.PlanFailureException;
 import jadex.micro.annotation.Agent;
 import jadex.micro.annotation.AgentBody;
 import jadex.quickstart.cleanerworld.environment.IChargingstation;
@@ -17,10 +19,7 @@ import jadex.quickstart.cleanerworld.environment.SensorActuator;
 import jadex.quickstart.cleanerworld.gui.SensorGui;
 
 /**
- *  First BDI agent with a goal and a plan.
- *  @author Alexander Pokahr
- *  @version 1.0 (2018/09/27)
- *
+ *  Separate Maintain and Target Conditions.
  */
 @Agent(type="bdi")	// This annotation makes the java class and agent and enabled BDI features
 public class CleanerBDIAgent
@@ -33,6 +32,9 @@ public class CleanerBDIAgent
 	/** Knowledge of the cleaner about itself (e.g. location and charge state). */
 	@Belief
 	private ICleaner	self	= actsense.getSelf();
+	
+	/** Knowledge of the cleaner about existing charging stations. */
+	@Belief	Set<IChargingstation>	chargingstations	= new LinkedHashSet<>();
 	
 	//-------- simple example behavior --------
 	
@@ -56,15 +58,18 @@ public class CleanerBDIAgent
 	/**
 	 *  A goal to patrol around in the museum.
 	 */
-	@Goal(recur=true, recurdelay=3000, randomselection=true)	// The goal annotation allows instances of a Java class to be dispatched as goals of the agent. 
+//	@Goal(recur=true, orsuccess=false, recurdelay=3000)
+	@Goal(recur=true, orsuccess=false, retrydelay=3000)	// Goal flags: variation 1
+//	@Goal(recur=true, randomselection=true, recurdelay=3000) // Goal flags: variation 2
+//	@Goal(orsuccess=false, excludemode=ExcludeMode.WhenFailed, randomselection=true, retrydelay=3000) // Goal flags: variation 3
+//	@Goal(recur=true, posttoall=true) // Goal flags: variation 4
 	class PerformPatrol {}
 	
 	/**
 	 *  A goal to recharge whenever the battery is low.
 	 */
-	@Goal(recur=true, recurdelay=0,
-		deliberation=@Deliberation(inhibits=PerformPatrol.class)	// Pause patroling while loading battery
-	)	// The goal annotation allows instances of a Java class to be dispatched as goals of the agent. 
+	@Goal(recur=true, recurdelay=3000, 
+		deliberation=@Deliberation(inhibits=PerformPatrol.class))	// Pause patrol goal while loading battery
 	class MaintainBatteryLoaded
 	{
 		@GoalMaintainCondition	// The cleaner aims to maintain the following expression, i.e. act to restore the condition, whenever it changes to false.
@@ -76,7 +81,7 @@ public class CleanerBDIAgent
 		@GoalTargetCondition	// Only stop charging, when this condition is true
 		boolean isBatteryFullyLoaded()
 		{
-			return self.getChargestate()>=0.8; // Charge until 80%
+			return self.getChargestate()>=0.9; // Charge until 90%
 		}
 	}
 	
@@ -89,52 +94,57 @@ public class CleanerBDIAgent
 	private void	performPatrolPlan()
 	{
 		// Follow a simple path around the four corners of the museum and back to the first corner.
+		System.out.println("Starting performPatrolPlan()");
 		actsense.moveTo(0.1, 0.1);
 		actsense.moveTo(0.1, 0.9);
 		actsense.moveTo(0.9, 0.9);
 		actsense.moveTo(0.9, 0.1);
 		actsense.moveTo(0.1, 0.1);
-		throw new PlanFailureException();
 	}
 
 	/**
 	 *  Declare a second plan for the PerformPatrol goal.
 	 */
-	@Plan(trigger=@Trigger(goals=PerformPatrol.class))	// The plan annotation makes a method or class a plan. The trigger states, when the plan should considered for execution.
+	@Plan(trigger=@Trigger(goals=PerformPatrol.class))
 	private void	performPatrolPlan2()
 	{
 		// Follow another path around the middle of the museum.
+		System.out.println("Starting performPatrolPlan2()");
 		actsense.moveTo(0.3, 0.3);
 		actsense.moveTo(0.3, 0.7);
 		actsense.moveTo(0.7, 0.7);
 		actsense.moveTo(0.7, 0.3);
 		actsense.moveTo(0.3, 0.3);
-		throw new PlanFailureException();
 	}
 	
 	/**
 	 *  Declare a third plan for the PerformPatrol goal.
 	 */
-	@Plan(trigger=@Trigger(goals=PerformPatrol.class))	// The plan annotation makes a method or class a plan. The trigger states, when the plan should considered for execution.
+	@Plan(trigger=@Trigger(goals=PerformPatrol.class))
 	private void	performPatrolPlan3()
 	{
 		// Follow a zig-zag path in the museum.
+		System.out.println("Starting performPatrolPlan3()");
 		actsense.moveTo(0.3, 0.3);
 		actsense.moveTo(0.7, 0.7);
 		actsense.moveTo(0.3, 0.7);
 		actsense.moveTo(0.7, 0.3);
 		actsense.moveTo(0.3, 0.3);
-		throw new PlanFailureException();
 	}
 	
 	/**
-	 * 
+	 *  Move to charging station and load battery.
 	 */
 	@Plan(trigger=@Trigger(goals=MaintainBatteryLoaded.class))
 	private void loadBattery()
 	{
+		System.out.println("Starting loadBattery() plan");
+		
+		// Move to first known charging station -> fails when no charging station known.
 		IChargingstation	chargingstation	= actsense.getChargingstations().iterator().next();
 		actsense.moveTo(chargingstation.getLocation());
-		actsense.recharge(chargingstation, 1.1);
+		
+		// Load until 100% (never reached, but plan is aborted when goal succeeds).
+		actsense.recharge(chargingstation, 1.0);
 	}
 }
