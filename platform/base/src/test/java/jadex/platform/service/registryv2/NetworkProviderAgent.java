3ca975c766bf7a1fbf30ed23cb71924d8b6df755
Alexander Pokahr <ap@actoron.com>
2018-11-08 15:03:15 +0100

    more debug

--- platform/base/src/test/java/jadex/platform/service/registryv2/NetworkProviderAgent.java
+++ platform/base/src/test/java/jadex/platform/service/registryv2/NetworkProviderAgent.java
@@ -1,6 +1,8 @@
 package jadex.platform.service.registryv2;
 
+import jadex.bridge.IInternalAccess;
 import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.AgentBody;
 import jadex.micro.annotation.ProvidedService;
 import jadex.micro.annotation.ProvidedServices;
 import jadex.micro.annotation.RequiredService;
@@ -12,5 +14,13 @@ import jadex.micro.annotation.RequiredService;
 @ProvidedServices(@ProvidedService(type=ITestService.class, scope=RequiredService.SCOPE_NETWORK))
 public class NetworkProviderAgent	implements ITestService
 {
-
+	@AgentBody
+	void body(IInternalAccess agent)
+	{
+		while(true)
+		{
+			System.out.println(agent+" alive");
+			agent.waitForDelay(500).get();
+		}
+	}
 }
