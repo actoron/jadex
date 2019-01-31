554c503d6e75624bac692c24cf88a67adf915748
Alexander Pokahr <pokahr@hsu-hh.de>
2019-01-31 12:53:05 +0100

    fix RS chart test junit ignore

--- android/webservice/src/test/java/jadex/webservice/examples/rs/chart/RSChartTest.java
+++ android/webservice/src/test/java/jadex/webservice/examples/rs/chart/RSChartTest.java
@@ -18,6 +18,7 @@ import jadex.commons.future.ThreadSuspendable;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
