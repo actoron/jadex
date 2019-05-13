ac32ccd785bf0d22b1ad00d9afc086061312a8c7
Lars <lb@actoron.com>
2019-05-13 18:38:45 +0200

    first version for path variables in REST

--- platform/webservice-desktop-jetty/src/test/java/jadex/webservice/pathvar/IMyService.java
+++ platform/webservice-desktop-jetty/src/test/java/jadex/webservice/pathvar/IMyService.java
@@ -1,35 +1,35 @@
 package jadex.webservice.pathvar;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 import jadex.bridge.service.annotation.Service;
 import jadex.commons.future.IFuture;
 
+/**
+ *  Interface for testing different parameter types.
+ */
 @Service
 public interface IMyService
 {
-	/**
-	 * 
-	 */
 	@Path("abc/{path}/method1")
 	public IFuture<String> method1(@PathParam("path") String path);
 	
-	/**
-	 * 
-	 */
 	@Path("renamed")
 	public IFuture<String> method2();
 	
-	/**
-	 * 
-	 */
 	@Path("abc/renamed")
 	public IFuture<String> method3();
 	
-	/**
-	 * 
-	 */
 	@Path("{p1}/abc/{p2}")
 	public IFuture<String> method4(@PathParam("p1") String p1, @PathParam("p2") String p2);
+	
+	@POST
+	@Path("abc/{path}/method5")
+	public IFuture<String> method5(@PathParam("path") String path);
+	
+	@Path("{p1}/xyz/")
+	public IFuture<String> method6(@PathParam("p1") String p1, @QueryParam("name") String name);
 }
