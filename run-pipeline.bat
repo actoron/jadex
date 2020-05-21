3c66bb4361175d49c3ab635d5a0937db3cb7ab4c
Alexander Pokahr <ap@actoron.com>
2020-05-22 00:15:11 +0200

    cleanup pipeline

--- run-pipeline.bat
+++ run-pipeline.bat
@@ -1,4 +1,4 @@
-@rem Run Gitlab pipeline on windows
+@rem Run Gitlab pipeline job on windows, e.g.: 'run-pipeline.bat build'
 
 @rem Convert script directory to unix form (e.g. '/C/mydata/myproject').
 @set MYPATH=%~dp0
@@ -10,6 +10,6 @@
 @rem https://gitlab.com/gitlab-org/gitlab-runner/-/issues/4574#note_302916410
 @set RUNNER_ARGS=--docker-volumes %MYPATH%:/jadex_tmp --docker-privileged --pre-clone-script "umount /jadex1; mv jadex1 jadex1_copy; cp -r jadex_tmp jadex1"
 
-docker run --rm -it -v %MYPATH%:/jadex1:ro -v /var/run/docker.sock:/var/run/docker.sock -w /jadex1 gitlab/gitlab-runner:ubuntu-v12.10.2 exec docker test %RUNNER_ARGS%
+docker run --rm -it -v %MYPATH%:/jadex1:ro -v /var/run/docker.sock:/var/run/docker.sock -w /jadex1 gitlab/gitlab-runner:ubuntu-v12.10.2 exec docker %1 %RUNNER_ARGS%
 
 @pause
