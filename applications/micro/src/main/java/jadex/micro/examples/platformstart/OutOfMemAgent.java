f97ce8e0427a809e7e2102e5da6b0ca0e20858d5
Alexander Pokahr <ap@actoron.com>
2020-06-03 21:55:33 +0200

    Revert "component hard fail for auto restarting docker containers (e.g. on out of mem)" Instead use VM exit option (-XX:+ExitOnOutOfMemoryError) This reverts commit 87a7ea9e16689141f89a5c98bfe6728b4ce5ce06.

--- applications/micro/src/main/java/jadex/micro/examples/platformstart/OutOfMemAgent.java
+++ applications/micro/src/main/java/jadex/micro/examples/platformstart/OutOfMemAgent.java
@@ -1,12 +1,13 @@
 package jadex.micro.examples.platformstart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jadex.base.IPlatformConfiguration;
 import jadex.base.PlatformConfigurationHandler;
 import jadex.base.Starter;
-import jadex.bridge.IExternalAccess;
 import jadex.bridge.IInternalAccess;
 import jadex.bridge.service.annotation.OnStart;
-import jadex.commons.future.Future;
 import jadex.commons.future.IFuture;
 import jadex.micro.annotation.Agent;
 
@@ -16,8 +17,11 @@ import jadex.micro.annotation.Agent;
 @Agent
 public class OutOfMemAgent
 {
-	/** The memory hog. */
-	byte[]	memhog	= new byte[1024];
+	/** Amount of memory to hog in each step. */
+	static final int HOGSIZE	= 1024*1024;	// 1 MiB
+	
+	/** List to collect and keep the memory hogs. */
+	List<byte[]>	memhog	= new ArrayList<byte[]>();
 	
 	/**
 	 *  The agent body.
@@ -27,8 +31,7 @@ public class OutOfMemAgent
 	{
 		agent.repeatStep(1000, 1000, ia ->
 		{
-			memhog	= new byte[memhog.length*2];
-			System.out.println("Mem usage is "+memhog.length+ " byte.");
+			memhog.add(new byte[HOGSIZE]);
 			return IFuture.DONE;
 		});
 	}
@@ -38,16 +41,8 @@ public class OutOfMemAgent
 	 */
 	public static void main(String[] args)
 	{
-		try
-		{
-			IPlatformConfiguration	config	= PlatformConfigurationHandler.getMinimalComm();
-			IExternalAccess	platform	= Starter.createPlatform(config, args).get();
-			IExternalAccess	agent	= platform.addComponent(new OutOfMemAgent()).get();
-			agent.subscribeToResults().get();
-		}
-		catch(Throwable t)
-		{
-			System.exit(1);
-		}
+		IPlatformConfiguration	config	= PlatformConfigurationHandler.getMinimalComm();
+		config.addComponent(OutOfMemAgent.class);
+		Starter.createPlatform(config, args).get();
 	}
 }
--- platform/bridge/src/main/java/jadex/bridge/component/impl/ExecutionComponentFeature.java
+++ platform/bridge/src/main/java/jadex/bridge/component/impl/ExecutionComponentFeature.java
@@ -1439,15 +1439,14 @@ public class ExecutionComponentFeature	extends	AbstractComponentFeature implemen
 //							if(!step.getSecondEntity().hasResultListener())
 //							{
 								// Todo: fail fast vs robust components.
-								killComponent(fex instanceof Exception ? (Exception)fex : new RuntimeException(fex));
-//								StringWriter	sw	= new StringWriter();
-//								fex.printStackTrace(new PrintWriter(sw));
-//								getComponent().getLogger().severe("Component step failed: "+step.getStep()+"\n"+sw);
-//								
-//								if(DEBUG && stepadditions!=null && stepadditions.containsKey(step.getStep()))
-//								{
-//									stepadditions.get(step.getStep()).printStackTrace();
-//								}
+								StringWriter	sw	= new StringWriter();
+								fex.printStackTrace(new PrintWriter(sw));
+								getComponent().getLogger().severe("Component step failed: "+step.getStep()+"\n"+sw);
+								
+								if(DEBUG && stepadditions!=null && stepadditions.containsKey(step.getStep()))
+								{
+									stepadditions.get(step.getStep()).printStackTrace();
+								}
 //							}
 //						}
 //						
