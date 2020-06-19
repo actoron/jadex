c5db6ddd6cb1d96d6458313636915e7247bb39ee
Alexander Pokahr <ap@actoron.com>
2020-06-19 15:26:57 +0200

    update windows crypt api

--- util/security/src/main/java/jadex/commons/security/WindowsEntropyApi.java
+++ util/security/src/main/java/jadex/commons/security/WindowsEntropyApi.java
@@ -4,10 +4,7 @@ import com.sun.jna.Function;
 import com.sun.jna.Memory;
 import com.sun.jna.NativeLibrary;
 import com.sun.jna.Pointer;
-import com.sun.jna.WString;
-import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinDef.ULONG;
-import com.sun.jna.platform.win32.WinDef.ULONGByReference;
 
 import jadex.commons.SUtil;
 
@@ -18,7 +15,7 @@ import jadex.commons.SUtil;
 public class WindowsEntropyApi
 {
 	/** Library name used to access the API */
-	public static final String WIN_LIB_NAME = "Advapi32";
+	public static final String WIN_LIB_NAME = "Bcrypt";
 	
 	/** Default provider to use. */
 	public static final int PROV_RSA_FULL = 1;
@@ -33,27 +30,10 @@ public class WindowsEntropyApi
 		byte[] ret = null;
 		try
 		{
-			ULONGByReference hProv = new WinDef.ULONGByReference();
-//			System.out.println("Pointer1: " + hProv.getValue().longValue());
-			ULONGByReference hNKProv = new WinDef.ULONGByReference();
-			boolean acquired = CryptAcquireContextW(hProv.getPointer(), null, null, PROV_RSA_FULL, 0);
-			boolean nkacquired = false;
-			if (!acquired)
+			Memory buf = new Memory(numbytes);
+			if (BCryptGenRandom(buf, new ULONG(numbytes)))
 			{
-				nkacquired = CryptAcquireContextW(hNKProv.getPointer(), null, null, PROV_RSA_FULL, 8);
-				acquired = CryptAcquireContextW(hProv.getPointer(), null, null, PROV_RSA_FULL, 0);
-			}
-			if (acquired)
-			{
-//				System.out.println("Pointer2: " + hProv.getValue().longValue());
-				Memory buf = new Memory(numbytes);
-				if (CryptGenRandom(hProv.getValue(), numbytes, buf))
-				{
-					CryptReleaseContext(hProv.getValue(), 0);
-					if (nkacquired)
-						CryptReleaseContext(hNKProv.getValue(), 0);
-					ret = buf.getByteArray(0, numbytes);
-				}
+				ret = buf.getByteArray(0, numbytes);
 			}
 		}
 		catch (Throwable e)
@@ -63,29 +43,12 @@ public class WindowsEntropyApi
 		return ret;
 	}
 	
-	/** Acquires the crypt context. */
-	private static boolean CryptAcquireContextW(Pointer phProv, WString pszContainer, WString pszProvider, int dwProvType, int dwFlags)
-	{
-		Function f = NativeLibrary.getInstance(WIN_LIB_NAME).getFunction("CryptAcquireContextW");
-		return f.invokeInt(new Object[] { phProv, pszContainer, pszProvider, dwProvType, dwFlags }) != 0;
-	}
-	
-	/** Releases the crypt context. */
-	private static boolean CryptReleaseContext(ULONG hProv, int dwFlags)
-	{
-		Function f = NativeLibrary.getInstance(WIN_LIB_NAME).getFunction("CryptReleaseContext");
-		return f.invokeInt(new Object[] { hProv, dwFlags }) != 0;
-	}
-	
 	/** Generates random data. */
-	private static boolean CryptGenRandom(ULONG hProv, int dwLen, Pointer pbBuffer)
+	private static boolean BCryptGenRandom(Pointer pbBuffer, ULONG cbBuffer)
 	{
-		Function f = NativeLibrary.getInstance(WIN_LIB_NAME).getFunction("CryptGenRandom");
-		boolean ret = f.invokeInt(new Object[] { hProv, dwLen, pbBuffer }) != 0;
-//		if (!ret)
-//		{
-//			System.out.println("Last Native Error: " + Native.getLastError());
-//		}
+		ULONG dwFlags = new ULONG(2);
+		Function f = NativeLibrary.getInstance(WIN_LIB_NAME).getFunction("BCryptGenRandom");
+		boolean ret = f.invokeInt(new Object[] { null, pbBuffer, cbBuffer, dwFlags }) == 0;
 		return ret;
 	}
 }
