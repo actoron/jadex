3feba9890780c04809d1ba7462b8713c97c511aa
Alexander Pokahr <ap@actoron.com>
2021-01-27 18:12:36 +0100

    calc asynchronously for being able to publish intermediate results

--- applications/micro/src/main/java/jadex/micro/examples/mandelbrot_new/CalculateService.java
+++ applications/micro/src/main/java/jadex/micro/examples/mandelbrot_new/CalculateService.java
@@ -1,9 +1,13 @@
 package jadex.micro.examples.mandelbrot_new;
 
+import java.util.Collection;
+
 import jadex.bridge.IInternalAccess;
 import jadex.bridge.service.annotation.Service;
 import jadex.bridge.service.annotation.ServiceComponent;
 import jadex.bridge.service.annotation.Timeout;
+import jadex.commons.future.ExceptionDelegationResultListener;
+import jadex.commons.future.IFuture;
 import jadex.commons.future.IIntermediateFuture;
 import jadex.commons.future.IntermediateFuture;
 
@@ -34,85 +38,98 @@ public class CalculateService implements ICalculateService
 	{
 		IntermediateFuture<PartDataChunk> ret = new IntermediateFuture<>();
 		
-		//long start = System.currentTimeMillis();
-		//System.out.println("calc start: "+data.getId()+" "+ag.getId());
-		
-		agent.setTaskId(data.getId());
-		ret.addIntermediateResult(new PartDataChunk(0, null, 0, 0, ag.getId()));
-		
-		// This code iterates over the area in a bounding boxes
-		// If a complete bounding box has is in the set the rest
-		// is just set to -1 without calculation. This is more
-		// efficient for areas with 'much black'.
-		
-		double stepx = (data.getXEnd()-data.getXStart())/data.getSizeX();
-		double stepy = (data.getYEnd()-data.getYStart())/data.getSizeY();
-		
-		int xstart = 0;
-		int xend = data.getSizeX()-1;
-		int ystart = 0;
-		int yend = data.getSizeY()-1;
-		boolean justfill = false;
-		short fillcol = -2;
-		
-		int numchunks = data.getChunkCount();
-		//System.out.println("Chunk count: "+numchunks);
-
-		int	size = data.getSizeX()*data.getSizeY();
-		
-		//int amount = (yend-ystart+1)*(xend-xstart+1);
-		int perchunk = size/numchunks;
-		int lastchunk = perchunk+size-(perchunk*numchunks);
-		
-		int cnt = 0;
-		int pointcnt = 0;
-		int chunkcnt = 0;
-		short[] chunk = new short[perchunk];
-		int xcs = xstart;
-		int ycs = ystart;
-		for(int yi=ystart; yi<=yend; yi++)
+		// Do calc on step so intermediate future can be returned while still calculating
+		IFuture<Void>	calcstep	= agent.agent.scheduleStep(ia ->
 		{
-			//short[] res = new short[data.getSizeX()];
-			for(int xi=xstart; xi<=xend; xi++)
+			long start = System.currentTimeMillis();
+//			System.out.println("calc start: "+data.getId()+" "+ag.getId());
+			
+			agent.setTaskId(data.getId());
+			ret.addIntermediateResult(new PartDataChunk(0, null, 0, 0, ag.getId()));
+			
+			// This code iterates over the area in a bounding boxes
+			// If a complete bounding box has is in the set the rest
+			// is just set to -1 without calculation. This is more
+			// efficient for areas with 'much black'.
+			
+			double stepx = (data.getXEnd()-data.getXStart())/data.getSizeX();
+			double stepy = (data.getYEnd()-data.getYStart())/data.getSizeY();
+			
+			int xstart = 0;
+			int xend = data.getSizeX()-1;
+			int ystart = 0;
+			int yend = data.getSizeY()-1;
+			boolean justfill = false;
+			short fillcol = -2;
+			
+			int numchunks = data.getChunkCount();
+			//System.out.println("Chunk count: "+numchunks);
+	
+			int	size = data.getSizeX()*data.getSizeY();
+			
+			//int amount = (yend-ystart+1)*(xend-xstart+1);
+			int perchunk = size/numchunks;
+			int lastchunk = perchunk+size-(perchunk*numchunks);
+			
+			int cnt = 0;
+			int pointcnt = 0;
+			int chunkcnt = 0;
+			short[] chunk = new short[perchunk];
+			int xcs = xstart;
+			int ycs = ystart;
+			for(int yi=ystart; yi<=yend; yi++)
 			{
-				//res[xi] = justfill? fillcol: data.getAlgorithm().determineColor(data.getXStart()+xi*stepx, data.getYStart()+yi*stepy, data.getMax());
-				chunk[pointcnt] = justfill? fillcol: data.getAlgorithm().determineColor(data.getXStart()+xi*stepx, data.getYStart()+yi*stepy, data.getMax());
-				if(!justfill && xi==xstart)
-					fillcol = chunk[pointcnt];
-				pointcnt++;
-				cnt++;
-				
-				if((chunkcnt==numchunks-1 && lastchunk==pointcnt) || (chunkcnt!=numchunks-1 && pointcnt==perchunk))
+				//short[] res = new short[data.getSizeX()];
+				for(int xi=xstart; xi<=xend; xi++)
 				{
-					int psize = (yend-ystart+1)*(xend-xstart+1);
-					//System.out.println("cnt: "+cnt+" "+size+" "+psize);
-					ret.addIntermediateResult(new PartDataChunk(cnt*100/size, chunk, xcs, ycs, ag.getId()));
-					pointcnt = 0;
-					chunkcnt++;
-					chunk = new short[chunkcnt==numchunks-1? lastchunk: perchunk];
-					xcs = xi+1;
-					ycs = yi;
-					if(xcs>xend)
+					//res[xi] = justfill? fillcol: data.getAlgorithm().determineColor(data.getXStart()+xi*stepx, data.getYStart()+yi*stepy, data.getMax());
+					chunk[pointcnt] = justfill? fillcol: data.getAlgorithm().determineColor(data.getXStart()+xi*stepx, data.getYStart()+yi*stepy, data.getMax());
+					if(!justfill && xi==xstart)
+						fillcol = chunk[pointcnt];
+					pointcnt++;
+					cnt++;
+					
+					if((chunkcnt==numchunks-1 && lastchunk==pointcnt) || (chunkcnt!=numchunks-1 && pointcnt==perchunk))
 					{
-						xcs = xstart;
-						ycs++;
-					}
-				}	
-				//System.out.print(res[xi]+"-");
+	//					int psize = (yend-ystart+1)*(xend-xstart+1);
+						Long dur = (System.currentTimeMillis()-start)/1000;
+//						System.out.println("calc addIntermediateResult: "+data.getId()+" secs="+dur);
+	//					System.out.println("cnt: "+cnt+" "+size+" "+psize);
+						ret.addIntermediateResult(new PartDataChunk(cnt*100/size, chunk, xcs, ycs, ag.getId()));
+						pointcnt = 0;
+						chunkcnt++;
+						chunk = new short[chunkcnt==numchunks-1? lastchunk: perchunk];
+						xcs = xi+1;
+						ycs = yi;
+						if(xcs>xend)
+						{
+							xcs = xstart;
+							ycs++;
+						}
+					}	
+					//System.out.print(res[xi]+"-");
+				}
+				//System.out.println();
 			}
-			//System.out.println();
-		}
+			
+			agent.setTaskId(null);
+			//data.setData(res);
+			//ret.setResult(data);
+			
+			Long dur = (System.currentTimeMillis()-start)/1000;
+//			System.out.println("calc end: "+data.getId()+" secs="+dur);			
+			return IFuture.DONE;
+		});
 		
-		agent.setTaskId(null);
-		//data.setData(res);
-		//ret.setResult(data);
-		
-		//Long dur = (System.currentTimeMillis()-start)/1000;
-		//System.out.println("calc end: "+data.getId()+" "+dur);
-		
-		ret.setFinished();
-		
-		//System.out.println("calc finished: "+data.getId()+" "+ag.getId());
+		calcstep.addResultListener(new ExceptionDelegationResultListener<Void, Collection<PartDataChunk>>(ret)
+		{
+			@Override
+			public void customResultAvailable(Void result) throws Exception
+			{
+				ret.setFinished();				
+//				System.out.println("calc finished: "+data.getId()+" "+ag.getId());
+			}
+		});
 		
 		return ret;
 	}
