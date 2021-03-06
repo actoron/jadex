660bd2e81a308869432231270a86dfe718f32999
Alexander Pokahr <ap@actoron.com>
2021-03-07 00:02:23 +0100

    add missing service annotation

--- platform/base/src/test/java/jadex/platform/service/expressionscope/IExpressionScopeService.java
+++ platform/base/src/test/java/jadex/platform/service/expressionscope/IExpressionScopeService.java
@@ -1,8 +1,11 @@
 package jadex.platform.service.expressionscope;
 
+import jadex.bridge.service.annotation.Service;
+
 /**
  *  Test service.
  */
+@Service
 public interface IExpressionScopeService
 {
 }
