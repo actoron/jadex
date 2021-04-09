15086349617674ab697ea295d053ac1d2495add5
Alexander Pokahr <ap@actoron.com>
2021-04-09 13:18:29 +0200

    continue testloop and store failures along the way

--- testloop.bat
+++ testloop.bat
@@ -8,15 +8,22 @@
 )
 
 call gradlew clean -PbuildDir=%BUILD_DIR%
-set jadex_timeout=180000
+@rem set jadex_timeout=60000
 :test_loop
 CMD /C gradlew -Pdist=publishdists test -x javadoc --continue -PbuildDir=%BUILD_DIR%
 
 @set builderror=%ERRORLEVEL%
 @if "%builderror%" NEQ "0" (
-    echo Generating Test Report... & call gradlew testReport -PbuildDir=%BUILD_DIR% >NUL & start "" %BUILD_DIR%"\testreport\index.html"
-) else (
-    goto test_loop
+    @rem on error create a report and copy it to a new folder
+    echo Generating Test Report... & call gradlew testReport -PbuildDir=%BUILD_DIR% >NUL
+    set /a num=1
+    :dirloop
+    if exist "reports\report-%num%" (
+        set /a num=%num%+1
+        goto dirloop
+    )
+    mkdir "reports\report-%num%"
+    robocopy "%BUILD_DIR%\testreport" "reports\report-%num%" /e
+    start "" "reports\report-%num%\index.html"
 )
-
-@pause
+goto test_loop
